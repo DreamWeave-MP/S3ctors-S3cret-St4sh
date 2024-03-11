e67f74c77a506d1c6cec395241aee66c316c6c25
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2024-03-10 20:03:18 -0500

    Rename, update

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
-# OpenMW Mod Template
+# MOMW Mod Template
 
-A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with. Made to be used with [this modders' guide](https://modding-openmw.com/guides/modders/).
+A template for building and distributing Morrowind mods, designed to be used as a base for new projects to quickly get going with. Made to be used with [this modders' guide](https://modding-openmw.com/guides/modders/).
+
+#### Credits
+
+Author: **Your Name Here**
 
 #### Installation
 
-**OpenMW 0.48 or newer is required!**
+1. Download the mod from [this URL](https://modding-openmw.gitlab.io/momw-mod-template/)
+1. Extract the zip to a location of your choosing, examples below:
 
-[This page](https://modding-openmw.com/getting-started/managing-mods/) explains how to install OpenMW mods.
+        # Windows
+        C:\games\OpenMWMods\momw-mod-template
+
+        # Linux
+        /home/username/games/OpenMWMods/momw-mod-template
+
+        # macOS
+        /Users/username/games/OpenMWMods/momw-mod-template
+
+1. Add the appropriate data path to your `opemw.cfg` file (e.g. `data="C:\games\OpenMWMods\momw-mod-template"`)
+1. Add `content=momw-mod-template.omwaddon` and `content=momw-mod-template.omwscripts` to your load order in `openmw.cfg` or enable them via OpenMW-Launcher
+
+#### Report A Problem
+
+If you've found an issue with this mod, or if you simply have a question, please use one of the following ways to reach out:
+
+* [Open an issue on GitLab](https://gitlab.com/modding-openmw/momw-mod-template/-/issues)
+* Email `YOUR EMAIL HERE`
+* Contact the author on Discord: `@YOUR DISCORD HERE`
--- fork-tool.sh
+++ fork-tool.sh
@@ -33,75 +33,36 @@ mod_id=${name//[[:space:]]/}
 this_year=$(date +%Y)
 
 # Rename dirs and things
-mv ./l10n/OpenMWModTemplate ./l10n/"${mod_id}"
-mv ./scripts/OpenMWModTemplate ./scripts/"${mod_id}"
-mv OpenMWModTemplate.omwscripts "${mod_id}".omwscripts
+mv ./l10n/MOMWModTemplate ./l10n/"${mod_id}"
+mv ./scripts/MOMWModTemplate ./scripts/"${mod_id}"
+mv MOMWModTemplate.omwscripts "${mod_id}".omwscripts
 
 # Find and replace
-# .gitlab-ci.yml:9:  project: openmw-mod-template
-sed -i "s|openmw-mod-template|${mod_id}|g" .gitlab-ci.yml
+sed -i "s|momw-mod-template|${mod_id}|g" .gitlab-ci.yml
+sed -i "s|momw-mod-template|${mod_id}|g;s|MOMWModTemplate|${mod_id}|g" pkg.sh
+sed -i "s|MOMW Mod Template|${name}|;s|modding-openmw|${acct}|;s|momw-mod-template|${mod_id}|" CHANGELOG.md
+sed -i "s|A template for building and distributing Morrowind mods, designed to be used as a base for new projects to quickly get going with.|${desc}|" ./l10n/"${mod_id}"/en.yaml
+sed -i "s|2022|${this_year}|;s|MOMW Mod Template|${name}|" LICENSE
+sed -i "s|MOMWModTemplate|${mod_id}|" "${mod_id}".omwscripts
+sed -i "s|MOMW Mod Template|${name}|;s|A template for building and distributing Morrowind mods, designed to be used as a base for new projects to quickly get going with. Made to be used with \[this modders' guide\](https://modding-openmw.com/guides/modders/).|${desc}|" README.md
+sed -i "s|MOMWModTemplate|${mod_id}|g;s|MOMW Mod Template|%{name}|g" ./scripts/"${mod_id}"/player.lua
+sed -i "s|MOMW Mod Template|${name}|g;s|A template for building and distributing Morrowind mods, designed to be used as a base for new projects to quickly get going with.${desc}|g;s|momw-mod-template|${mod_id}|" web/soupault.toml
+sed -i "s|A template for building and distributing Morrowind mods, designed to be used as a base for new projects to quickly get going with. Made to be used with <a href=\"https://modding-openmw.com/guides/modders/\"> this modders' guide</a>.|${desc}|" web/site/index.html
+sed -i "s|A template for building and distributing Morrowind mods, designed to be used as a base for new projects to quickly get going with.|${desc}|;s|MOMW Mod Template|${name}|g;s|modding-openmw|${acct}|g;s|momw-mod-template|${mod_id}|g" web/templates/main.html
 
-# pkg.sh:8:file_name=openmw-mod-template.zip
-# pkg.sh:14:zip --must-match --recurse-paths ${file_name} scripts CHANGELOG.md LICENSE README.md OpenMWModTemplate.omwscripts version.txt
-sed -i "s|openmw-mod-template|${mod_id}|g;s|OpenMWModTemplate|${mod_id}|g" pkg.sh
+echo rm -rf .git
 
-# CHANGELOG.md:1:## OpenMW Mod Template Changelog
-# CHANGELOG.md:7:<!--[Download Link](https://gitlab.com/modding-openmw/openmw-mod-template/-/packages/TODO)-->
-sed -i "s|OpenMW Mod Template|${name}|;s|modding-openmw|${acct}|;s|openmw-mod-template|${mod_id}|" CHANGELOG.md
-
-# l10n/OpenMWModTemplate/en.yaml:1:modDescription: A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with.
-sed -i "s|A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with.|${desc}|" ./l10n/"${mod_id}"/en.yaml
-
-# LICENSE:3:Copyright (c) 2022 OpenMW Mod Template Authors
-sed -i "s|2022|${this_year}|;s|OpenMW Mod Template|${name}|" LICENSE
-
-# openmw-mod-template.omwscripts:1:PLAYER: scripts/openmw-mod-template/player.lua
-sed -i "s|OpenMWModTemplate|${mod_id}|" "${mod_id}".omwscripts
-
-# README.md:1:# OpenMW Mod Template
-# README.md:3:A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with. Made to be used with [this modders' guide](https://modding-openmw.com/guides/modders/).
-sed -i "s|OpenMW Mod Template|${name}|;s|A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with. Made to be used with \[this modders' guide\](https://modding-openmw.com/guides/modders/).|${desc}|" README.md
-
-# scripts/OpenMWModTemplate/player.lua:15:local MOD_ID = "OpenMWModTemplate"
-# scripts/OpenMWModTemplate/player.lua:32:    name = "OpenMW Mod Template",
-# scripts/OpenMWModTemplate/player.lua:47:    name = "OpenMW Mod Template",
-# scripts/OpenMWModTemplate/player.lua:101:    -- I.OpenMWModTemplate.SayHello()
-sed -i "s|OpenMWModTemplate|${mod_id}|g;s|OpenMW Mod Template|%{name}|g" ./scripts/"${mod_id}"/player.lua
-
-# web/soupault.toml:28:  default = "OpenMW Mod Template &mdash; A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with"
-# web/soupault.toml:29:  append = " | OpenMW Mod Template &mdash; A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with"
-# web/soupault.toml:73:  project = "openmw-mod-template"
-sed -i "s|OpenMW Mod Template|${name}|g;s|A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with|${desc}|g;s|openmw-mod-template|${mod_id}|" web/soupault.toml
-
-# web/site/index.html:1:<p>A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with. Made to be used with <a href="https://modding-openmw.com/guides/modders/"> this modders' guide</a>.</p>
-# web/site/index.html:3:  <img src="/img/openmw-mod-template.gif" title="A short gif of the mod in action" />
-sed -i "s|A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with. Made to be used with <a href=\"https://modding-openmw.com/guides/modders/\"> this modders' guide</a>.|${desc}|" web/site/index.html
-
-# web/site/index.html:3:  <img src="/img/openmw-mod-template.gif" title="A short gif of the mod in action" />
-# The match above won't be replaced, the user will want to take a new screenshot/gif and replace it by hand.
-
-# web/templates/main.html:6:    <meta name="description" content="A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with.">
-# web/templates/main.html:7:    <meta name="author" content="OpenMW Mod Template Authors">
-# web/templates/main.html:15:    <h1 class="center">OpenMW Mod Template</h1>
-# web/templates/main.html:23:      <a class="button" id="download" title="Download the latest release of OpenMW Mod Template">Download</a>
-# web/templates/main.html:30:      <a id="download" href="https://gitlab.com/api/v4/projects/modding-openmw%2Fopenmw-mod-template/jobs/artifacts/master/raw/openmw-mod-template.zip?job=make" title="Dev builds may have newer features, but they will be less tested">Dev Build</a> (<a id="sha256" href="https://gitlab.com/api/v4/projects/modding-openmw%2Fopenmw-mod-template/jobs/artifacts/master/raw/openmw-mod-template.zip.sha256sum.txt?job=make" title="Verify the integrity of the file you downloaded">sha256</a>/<a id="sha512" href="https://gitlab.com/api/v4/projects/modding-openmw%2Fopenmw-mod-template/jobs/artifacts/master/raw/openmw-mod-template.zip.sha512sum.txt?job=make" title="Verify the integrity of the file you downloaded">sha512</a>)
-# web/templates/main.html:36:      <a href="https://gitlab.com/modding-openmw/openmw-mod-template/activity">Activity</a> | <a href="https://gitlab.com/modding-openmw/openmw-mod-template/-/issues">Report A Bug</a>
-# web/templates/main.html:43:    <div id="modProject" data-mod-project="openmw-mod-template"></div>
-sed -i "s|A template for building OpenMW mods, designed to be used as a base for new projects to quickly get going with.|${desc}|;s|OpenMW Mod Template|${name}|g;s|modding-openmw|${acct}|g;s|openmw-mod-template|${mod_id}|g" web/templates/main.html
-
-rm -rf .git
-
-git init
+echo git init
 
 # Inform the user how they can save their changes
 cat <<EOF
 
 Run this to save and push all changes:
 
-  git commit -am "This is my mod now!"
+  git commit -am "Initial commit of my new mod"
   git push
 
 EOF
 
 # Self destruct!
-rm -f "${0}"
+echo rm -f "${0}"
