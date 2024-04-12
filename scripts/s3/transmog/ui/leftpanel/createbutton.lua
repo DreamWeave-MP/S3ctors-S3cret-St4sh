a4aa0c495c6a527b0b4dc3407e33a33c315fdd1d
Dave Corley <smokydave@icloud.com>
2024-04-12 00:26:18 -0500

    FIX: Correct message on confirmation creation

--- scripts/s3/transmog/ui/leftpanel/createbutton.lua
+++ scripts/s3/transmog/ui/leftpanel/createbutton.lua
@@ -4,11 +4,17 @@ local common = require('scripts.s3.transmog.ui.common')
 
 local I = require('openmw.interfaces')
 
+local playerSettings = require('openmw.storage').playerSection('Settingss3_transmogMenuGroup'):asTable()
+
 return function()
   local button = common.createButton("Create")
   button.events.mousePress = async:callback(I.transmogActions.acceptTransmog)
   if not I.transmogActions.message.createExplain then
-    common.messageBoxSingleton("Create Explanation", "Press the create button or press enter to confirm the glamour.", 2)
+    common.messageBoxSingleton("Confirm Explanation",
+                               "Click the confirm button or press the "
+                               .. string.upper(playerSettings.TransmogMenuConfirm)
+                               .. " key to apply the glamour.\n"
+                               .. "Close with cancel or the escape key.", 1)
   end
   I.transmogActions.message.createExplain = true
   return button
