b49fb90e9d8164d914df165763f7f27187114216
Dave Corley <smokydave@icloud.com>
2024-04-12 23:02:40 -0500

    FEAT: Add basic tooltip for menu items; never destroy anything for improved perf, implement greeting on install, improves compatibility with the conole and keyboard, add a clear button in confirm menu to remove default name, add a background when hovering over menu items

--- scripts/s3/transmog/ui/leftpanel/cancelbutton.lua
+++ scripts/s3/transmog/ui/leftpanel/cancelbutton.lua
@@ -7,9 +7,9 @@ return function()
   local button = common.createButton("Cancel")
   button.events.mousePress = async:callback(
     function (_, _layout)
-      I.transmogActions.message.confirmScreen:destroy()
-      I.transmogActions.message.confirmScreen = nil
+      I.transmogActions.message.confirmScreen.layout.props.visible = false
       I.transmogActions.menu.layout.props.visible = true
+      I.transmogActions.message.confirmScreen:update()
       I.transmogActions.menu:update()
     end)
   return button
