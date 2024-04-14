60efff9d633b1d63bd3b6eaa41078c7e0adf9780
Dave Corley <smokydave@icloud.com>
2024-04-14 08:18:55 -0500

    FEAT: Add clear button to revert to default inventory type

---
+++ scripts/s3/transmog/ui/rightpanel/clearbutton.lua
@@ -0,0 +1,23 @@
+local async = require('openmw.async')
+local common = require('scripts.s3.transmog.ui.common')
+local self = require('openmw.self')
+local types = require('openmw.types')
+local ui = require('openmw.ui')
+
+local ItemContainer = require('scripts.s3.transmog.ui.rightpanel.itemcontainer')
+
+local I = require('openmw.interfaces')
+
+return function()
+  local button = common.createButton("Clear")
+  button.props.name =  "Clear Button"
+  button.events.mousePress = async:callback(function()
+    local itemContainer = I.transmogActions.itemContainer
+    itemContainer.userData = {[types.Armor] = true , [types.Clothing] = true}
+    itemContainer.content = ui.content(ItemContainer.updateContent(itemContainer.userData))
+    types.Actor.setEquipment(self, I.transmogActions.originalInventory)
+    common.resetPortraits()
+    I.transmogActions.menu:update()
+  end)
+  return button
+end
