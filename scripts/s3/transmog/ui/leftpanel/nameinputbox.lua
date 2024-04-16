e23152595dbd53e294954026166c268027388ce6
Dave Corley <smokydave@icloud.com>
2024-04-16 07:13:34 -0500

    FEAT: Start placing menu items directly into interface on construction. Also, make them self-referenceable objects. Do this with nameInputBox and confirm screen

---
+++ scripts/s3/transmog/ui/leftpanel/nameinputbox.lua
@@ -0,0 +1,42 @@
+local const = require('scripts.s3.transmog.ui.common').const
+local Object = require('scripts.s3.transmog.makeobject')
+
+local async = require('openmw.async')
+local ui = require('openmw.ui')
+local util = require('openmw.util')
+
+local I = require('openmw.interfaces')
+
+local NameInputBox = {
+  type = ui.TYPE.TextEdit,
+  events = {
+    textChanged = async:callback(
+      function (text, layout)
+        layout.props.text = text
+    end)
+  },
+  userData = {},
+  props = {
+    name = "Name Input",
+    -- Can we flex this or something?
+    size = util.vector2(384, 32),
+    textSize = const.FONT_SIZE,
+    textColor = const.TEXT_COLOR,
+    textAlignH = ui.ALIGNMENT.Center,
+    textAlignV = ui.ALIGNMENT.Center,
+    autoSize = false,
+  }
+}
+
+function NameInputBox.new(itemData)
+  print(type(NameInputBox))
+  NameInputBox.props.text = itemData.record.name
+  return Object:new(NameInputBox)
+end
+
+function NameInputBox:mogMenuEvent()
+  self.props.text = ""
+  I.transmogActions.MenuAliases()['confirm screen']:update()
+end
+
+return NameInputBox
