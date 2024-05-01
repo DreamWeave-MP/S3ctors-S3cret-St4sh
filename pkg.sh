0edd1f13e2fd188da642e8558b80006484bfe6c9
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2024-05-01 13:46:23 -0500

    Package localization files too

--- pkg.sh
+++ pkg.sh
@@ -11,6 +11,6 @@ cat > version.txt <<EOF
 Mod version: $(git describe --tags || git rev-parse --short HEAD)
 EOF
 
-zip --must-match --recurse-paths ${file_name} scripts CHANGELOG.md LICENSE README.md MOMWModTemplate.omwscripts version.txt
+zip --must-match --recurse-paths ${file_name} l10n scripts CHANGELOG.md LICENSE README.md MOMWModTemplate.omwscripts version.txt
 sha256sum ${file_name} > ${file_name}.sha256sum.txt
 sha512sum ${file_name} > ${file_name}.sha512sum.txt
