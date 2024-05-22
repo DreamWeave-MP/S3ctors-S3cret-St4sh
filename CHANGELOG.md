60e9f24ec904f574a78b41da441b5a0189196f32
Dave Corley <smokydave@icloud.com>
2024-05-22 03:07:16 -0500

    Pls no expl0d3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
-## MOMW Mod Template Changelog
+## s3ctors-s3cret-st4sh Changelog
 
 #### Version 1.0
 
 Initial version of the mod.
 
-<!--[Download Link](https://gitlab.com/modding-openmw/momw-mod-template/-/packages/TODO)-->
+<!--[Download Link](https://gitlab.com/modding-openmw/s3ctors-s3cret-st4sh/-/packages/TODO)-->
--- pkg.sh
+++ pkg.sh
@@ -5,12 +5,12 @@ set -eu
 # This script packages the project into a zip file.
 #
 
-file_name=momw-mod-template.zip
+file_name=s3ctors-s3cret-st4sh.zip
 
 cat > version.txt <<EOF
 Mod version: $(git describe --tags || git rev-parse --short HEAD)
 EOF
 
-zip --must-match --recurse-paths ${file_name} l10n scripts CHANGELOG.md LICENSE README.md MOMWModTemplate.omwscripts version.txt
+zip --must-match --recurse-paths ${file_name} l10n scripts CHANGELOG.md LICENSE README.md s3ctors-s3cret-st4sh.omwscripts version.txt
 sha256sum ${file_name} > ${file_name}.sha256sum.txt
 sha512sum ${file_name} > ${file_name}.sha512sum.txt
