9b7465bd6066a6c6b073668dc5672831447d92cd
Dave Corley <smokydave@icloud.com>
2024-05-24 10:36:16 -0500

    FEAT: Make archive script more generic

--- pkg.sh
+++ pkg.sh
@@ -11,16 +11,23 @@ cat > version.txt <<EOF
 Mod version: $(git describe --tags || git rev-parse --short HEAD)
 EOF
 
-zip --must-match \
-    --recurse-paths \
-    toolgun.zip \
-    toolgun/ \
+mods="toolgun"
+
+set -- mods
+
+for mod in $mods; do
 
 zip --must-match \
     --recurse-paths \
+    "$mod".zip \
+    "$mod"/
+
+done
+
+zip --must-match \
     ${file_name} \
     version.txt \
-    toolgun.zip
+    *.zip
 
 sha256sum ${file_name} > ${file_name}.sha256sum.txt
 sha512sum ${file_name} > ${file_name}.sha512sum.txt
