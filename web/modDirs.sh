ac1d03e23c6eb213f80d5b18cf17a18dcbc46144
Dave Corley <smokydave@icloud.com>
2024-05-26 18:17:40 -0500

    FIX: Make modDirs.sh POSIX-compliant

--- web/modDirs.sh
+++ web/modDirs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-find "$1" -maxdepth 1 -type d -printf "%f\n" \
-    | grep -v "\.\|.git\|web" \
+find "$1" -mindepth 1 -maxdepth 1 -type d \
+    | sed 's|.*/||' \
+    | grep -v ".git\|web" \
     | sort \
     | tr '\n' ' '
