a8d4a9b0735d6de73d6ab22e744684fe85c9f859
Dave Corley <smokydave@icloud.com>
2024-05-26 22:59:03 -0500

    Revert "FEAT: Dynamically generate modName element during build time"
    This reverts commit 573c6f8703bfad2681bf7e710b78cc1eef24e37f.

--- web/build.sh
+++ web/build.sh
@@ -25,35 +25,11 @@ fi
 
 # Changelog
 echo "Releases without a download link can be downloaded as a dev build from the link above." > site/changelog.md
-
-mods=$(./modDirs.sh ..)
-
-modimages=""
-
-set -- mods
-
-for mod in $mods; do
-
-grep -vi "# $mod" ../"$mod"/README.md >> site/"$mod".md
-grep -vi "# $mod Changelog" ../"$mod"/CHANGELOG.md >> site/"$mod"-changelog.md
-
-echo "<div id=\"modName\" data-mod-name=\""$mod"\"></div>" >> site/"$mod".md
-echo "<div id=\"modName\" data-mod-name=\""$mod"\"></div>" >> site/"$mod"-changelog.md
-
-modimages="$modimages<a href=\"./"$mod"/\"><img src=\"./img/"$mod".svg\" alt=\""$mod"\"/></a>+<br>+"
-
-done
+grep -v "## MOMW Mod Template" ../CHANGELOG.md >> site/changelog.md
 
 # Index
 # echo '<div class="center"><a href="/img/image.png"><img src="/img/image.png" title="The stats menu" /></a></div>' > site/index.md
-
-sed "s|<div id=\"modMarker\"></div>|$modimages|" ../README.md \
-    | tr '+' '\n' \
-    | grep -v "# s3ctors-s3cret-st4sh" >> site/index.md
-
-cat ../CHANGELOG.md >> site/s3ctors-s3cret-st4sh-changelog.md
-
-echo "<div id=\"modName\" data-mod-name=\"s3ctors-s3cret-st4sh\"></div>" >> site/s3ctors-s3cret-st4sh.md
-echo "<div id=\"modName\" data-mod-name=\"s3ctors-s3cret-st4sh\"></div>" >> site/s3ctors-s3cret-st4sh-changelog.md
+grep -v "# s3ctors-s3cret-st4sh" ../README.md >> site/index.md
+grep -v "# s3-toolgun" ../toolgun/README.md >> site/toolgun.md
 
 PATH=${soupault_path}:$PATH soupault "$@"
