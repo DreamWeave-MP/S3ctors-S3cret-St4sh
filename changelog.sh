fb9cbbe3bf580c32459345ce0215e06244d50ab2
Dave Corley <smokydave@icloud.com>
2024-05-29 05:49:58 -0500

    FEAT: Clean up changelog generator, remove dead code, collapse each version tag and make it underlined on hover

--- changelog.sh
+++ changelog.sh
@@ -2,21 +2,9 @@
 
 set -euo pipefail
 
-# Check if the current commit is associated with a tag
-
-
-# current_commit=$(git rev-parse HEAD)
-# tag_name=$(git describe --exact-match --tags $current_commit 2>/dev/null || true)
-
-# # If no tags are found, exit
-# if [ -z "$tag_name" ]; then
-#   echo "No tags found for: $current_commit"
-#   exit 0
-# fi
-
-# Get the name of the mod
+GITLAB_REPO_URL="https://gitlab.com/modding-openmw/s3ctors-s3cret-st4sh"
+previous_tag=""
 tag_prefix="$1"
-# tag_prefix=$(echo "$tag_name" | cut -d '-' -f 1)
 
 # Get all tagged releases for it, sorted by date
 tags=$(git tag --list "${tag_prefix}-*" --sort=-creatordate)
@@ -27,20 +15,28 @@ if [ -z "$tags" ]; then
   exit 0
 fi
 
-# Initialize previous tag as the first tag in the list
-previous_tag=""
-GITLAB_REPO_URL="https://gitlab.com/modding-openmw/s3ctors-s3cret-st4sh"
-
 # Iterate through the tags
 for tag in $tags; do
   if [ -n "$previous_tag" ]; then
-    # List all commits between the previous tag and the current tag
     version=$(echo "$previous_tag" | cut -d '-' -f 2)
-    echo -e "#### Version $version:\n  "
+
+    echo -e "
+<style>
+  h4 {
+    text-decoration: none;
+  }
+
+  h4:hover {
+    text-decoration: underline;
+  }
+</style>
+    "
+
+    echo -e "<details><summary><h4>Version $version:</h4></summary>"
     git log --pretty=format:"%h %s" "$tag".."$previous_tag" | while read -r commit_hash commit_message; do
-      echo "- [$commit_hash]($GITLAB_REPO_URL/commit/$commit_hash) - $commit_message  "
+      echo "- <a href="$GITLAB_REPO_URL/commit/$commit_hash">$commit_hash - $commit_message</a><br>"
     done
-    echo
+    echo -e "</details>\n"
   fi
   previous_tag=$tag
 done
