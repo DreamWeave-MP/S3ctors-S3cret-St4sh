94f60f766ec7f1c9ea769b9568bd1b63032f4b87
Dave Corley <smokydave@icloud.com>
2024-09-09 05:14:34 -0700

    FEAT(ProtectedTable): Add tostring method and expected value of failed inputs

--- chim_2090/scripts/s3/CHIM2090/protectedTable.lua
+++ chim_2090/scripts/s3/CHIM2090/protectedTable.lua
@@ -14,16 +14,31 @@ return function(inputTable, inputGroupName)
             string.format(
               [[%s Unauthorized table access when updating '%s' to '%s'.
 This table must be updated through its associated storage group: '%s'.
+Its expected value is '%s'.
 New values may not be added.]],
               modInfo.logPrefix,
               tostring(key),
               tostring(value),
-              inputGroupName),
+              inputGroupName,
+              settingValue),
             2)
         end
       end
       inputTable[key] = value
     end,
+    __tostring = function(_)
+      local parts = {}
+      for k, v in pairs(inputTable) do
+        local valueStr
+        if type(v) == "function" then
+          valueStr = "function()"
+        else
+          valueStr = tostring(v)
+        end
+        table.insert(parts, string.format("%s = %s", tostring(k), valueStr))
+      end
+      return string.format("CHIMManager{ %s }", table.concat(parts, ", "))
+    end
   }
   setmetatable(proxy, meta)
   return proxy
