2e5c46ba10d9b8c6cf4c2867696e9da7e0791c5a
Dave Corley <smokydave@icloud.com>
2024-09-11 13:25:52 -0700

    FEAT: Add magicka regen implementation

--- chim_2090/scripts/s3/CHIM2090/fatigueManager.lua
+++ chim_2090/scripts/s3/CHIM2090/fatigueManager.lua
@@ -1,4 +1,5 @@
 local core = require('openmw.core')
+local storage = require('openmw.storage')
 
 local s3lf = require('scripts.s3.lf')
 local modInfo = require('scripts.s3.CHIM2090.modInfo')
@@ -46,6 +47,18 @@ function FatigueManager:manageFatigue(dt)
   self:handleFatigueRegen(dt)
 end
 
+function FatigueManager.canRegenerateMagicka()
+  return s3lf.activeEffects():getEffect('stuntedmagicka').magnitude == 0
+end
+
+function FatigueManager.canRegenerateFatigue()
+  return true
+end
+
+function FatigueManager.canRegenerateHealth()
+  return true
+end
+
 function FatigueManager:getSleepRegenBase(sleepHours)
   local endFatigueMult = self.FatigueEndMult
   local totalEncumbrance = core.getGMST('fEncumbranceStrMult') * s3lf.strength.modified
@@ -53,6 +66,11 @@ function FatigueManager:getSleepRegenBase(sleepHours)
   local fatiguePerSecond = self.FatiguePerSecond + (self.FatigueReturnMult * regenPct)
   fatiguePerSecond = fatiguePerSecond * (endFatigueMult * s3lf.endurance.modified)
   return (fatiguePerSecond * sleepHours) * self.SleepFatigueMult
+end
+
+function FatigueManager:getSleepMagickaRegenBase(sleepHours)
+  local magicMult = storage.globalSection("SettingsGlobal" .. modInfo.name .. 'Sleep'):get('RestMagicMult')
+  return sleepHours * magicMult * (s3lf.intelligence.modified + s3lf.willpower.modified)
 end
 
 function FatigueManager.calculateNewDynamic(stat, multiplied, newTotal)
@@ -74,19 +92,37 @@ end
 function FatigueManager:sleepHealthRecoveryPlayer(prevHealth)
 end
 
-function FatigueManager:sleepMagickaRecoveryActor(prevMagicka)
+function FatigueManager:sleepMagickaRecoveryActor(sleepData)
+  if not self.canRegenerateMagicka() then return end
+
+  local totalFatigueRegen = self:getSleepMagickaRegenBase(sleepData.time)
+  local newTotal = s3lf.magicka.current + totalFatigueRegen
+  s3lf.magicka.current = math.min(s3lf.magicka.base, newTotal)
 end
 
-function FatigueManager:sleepMagickaRecoveryPlayer(prevMagicka)
+function FatigueManager:sleepMagickaRecoveryPlayer(sleepData)
+  if not self.canRegenerateMagicka() then return end
+
+  local totalMagickaRegen = self:getSleepMagickaRegenBase(sleepData.time)
+  local multiplier = sleepData.sleepMultiplier
+
+  local newTotal = s3lf.magicka.current + totalMagickaRegen
+  local multipliedMagicka = s3lf.magicka.base * multiplier
+
+  s3lf.magicka.current = self.calculateNewDynamic(s3lf.magicka, multipliedMagicka, newTotal)
 end
 
 function FatigueManager:sleepFatigueRecoveryActor(sleepData)
+  if not self.canRegenerateFatigue() then return end
+
   local totalFatigueRegen = self:getSleepRegenBase(sleepData.time)
   local newTotal = s3lf.fatigue.current + totalFatigueRegen
   s3lf.fatigue.current = math.min(s3lf.fatigue.base, newTotal)
 end
 
 function FatigueManager:sleepFatigueRecoveryPlayer(sleepData)
+  if not self.canRegenerateFatigue() then return end
+
   local totalFatigueRegen = self:getSleepRegenBase(sleepData.time)
 
   local multiplier = sleepData.sleepMultiplier
@@ -105,36 +141,50 @@ function FatigueManager:sleepFatigueRecoveryPlayer(sleepData)
 end
 
 function FatigueManager:sleepRecoveryActor(sleepData)
+  self:sleepMagickaRecoveryActor(sleepData)
   self:sleepFatigueRecoveryActor(sleepData)
 end
 
 function FatigueManager:sleepRecoveryPlayer(sleepData)
+  self:sleepMagickaRecoveryPlayer(sleepData)
   self:sleepFatigueRecoveryPlayer(sleepData)
 end
 
-function FatigueManager:sleepRecoveryVanilla()
+function FatigueManager:sleepRecoveryVanilla(sleepData)
   if self.UseVanillaFatigueFormula then
-    s3lf.fatigue.current = s3lf.fatigue.base
+    -- For health, do nothing, as it's handled engine-side based on endurance
+    if self.canRegenerateFatigue() then
+      s3lf.fatigue.current = s3lf.fatigue.base
+    end
+
+    if sleepData.restOrWait then
+      if self.canRegenerateMagicka() then
+        local magicMult = storage.globalSection("SettingsGlobal" .. modInfo.name .. 'Sleep'):get('RestMagicMult')
+        local magickaPerHour = s3lf.intelligence.modified * magicMult
+        s3lf.magicka.current = math.min(s3lf.magicka.base,
+                                        s3lf.magicka.current + (magickaPerHour * sleepData.time))
+      end
+    end
     return true
   end
   return false
 end
 
 function FatigueManager.sleepActorHandler(sleepData)
-  assert(sleepData and sleepData.time
-         and sleepData.actorStats, 's3ChimFatigue_SleepActor event requires a time value')
+  assert(sleepData and sleepData.time and sleepData.restOrWait ~= nil
+         and sleepData.oldHealth, 's3ChimFatigue_SleepActor event requires a time value')
 
-  if not FatigueManager:sleepRecoveryVanilla() then
+  if not FatigueManager:sleepRecoveryVanilla(sleepData) then
     FatigueManager:sleepRecoveryActor(sleepData)
   end
 end
 
 function FatigueManager.sleepPlayerHandler(sleepData)
-  assert(sleepData and sleepData.time
-         and sleepData.sleepMultiplier and sleepData.actorStats,
+  assert(sleepData and sleepData.time and sleepData.restOrWait ~= nil
+         and sleepData.oldHealth and sleepData.sleepMultiplier,
          's3ChimFatigue_SleepPlayer event requires a time value and a sleep multiplier')
 
-  if not FatigueManager:sleepRecoveryVanilla() then
+  if not FatigueManager:sleepRecoveryVanilla(sleepData) then
     FatigueManager:sleepRecoveryPlayer(sleepData)
   end
 end
