95a7bdbf7dbedfa41234b51272c5d517c6f27e12
Dave Corley <smokydave@icloud.com>
2025-02-10 22:10:20 -0700

    CHIM: Fix settings page by moving main registration into a MENU script

---
+++ chim_2090/scripts/s3/CHIM2090/settingsPage.lua
@@ -0,0 +1,14 @@
+local modInfo = require('scripts.s3.CHIM2090.modInfo')
+local I = require('openmw.interfaces')
+
+I.Settings.registerPage {
+  key = modInfo.name,
+  l10n = modInfo.l10nName,
+  name = "CHIM 2090",
+  description = "Manages actor fatigue, carry weight, hit chance, and strength in combat."
+}
+
+print(string.format("%s loaded version %s. Thank you for playing %s! <3",
+                    modInfo.logPrefix,
+                    modInfo.version,
+                    modInfo.name))
