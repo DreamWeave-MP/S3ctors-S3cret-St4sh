90ef531df7aeaa0805f353b782e2c0d0c696751d
Dave Corley <smokydave@icloud.com>
2025-02-10 23:03:32 -0700

    FEAT: Add template cells to item rows

--- s3ui/scripts/s3/transmog/ui/inventorycontainer/itemrow.lua
+++ s3ui/scripts/s3/transmog/ui/inventorycontainer/itemrow.lua
@@ -1,6 +1,7 @@
 local ui = require('openmw.ui')
+local util = require('openmw.util')
 
--- local common = require('scripts.s3.transmog.ui.common')
+local common = require('scripts.s3.transmog.ui.common')
 local sizes = require('scripts.s3.transmog.const.size')
 
 local I = require('openmw.interfaces')
@@ -32,7 +33,7 @@ function ItemRow.new(typedInventory, itemIndex)
   newRow.type = ui.TYPE.Flex
   newRow.props = {
     name = "Right Panel: Item Row ",
-    -- horizontal = true,
+    horizontal = true,
     -- autoSize = false,
     relativeSize = sizes.RPANEL.INVENTORY_ROW,
   }
@@ -40,20 +41,29 @@ function ItemRow.new(typedInventory, itemIndex)
     grow = 1,
     stretch = 1,
   }
-  newRow.content = {}
 
   newRow.props.name = newRow.props.name .. itemIndex
   local objectData = newRow.getItemData(typedInventory, itemIndex)
 
   if objectData then
-    newRow.content[#newRow.content + 1] = ItemPortrait.new(objectData)
+    newRow.content = {
+      ItemPortrait.new(objectData),
+      common.templateImage(util.color.hex('00ff00')),
+      common.templateImage(util.color.hex('ff0000')),
+      common.templateImage(util.color.hex('0000ff')),
+      common.templateImage(util.color.hex('ffff00')),
+      common.templateImage(util.color.hex('00ffff')),
+      common.templateImage(util.color.hex('ffffff')),
+    }
   else
-    newRow.content[#newRow.content + 1] = {
-      template = I.MWUI.templates.Interval,
-      external = {
-        grow = 1,
-        stretch = 1,
-      },
+    newRow.content = {
+      {
+        template = I.MWUI.templates.Interval,
+        external = {
+          grow = 1,
+          stretch = 1,
+        },
+      }
     }
   end
 
