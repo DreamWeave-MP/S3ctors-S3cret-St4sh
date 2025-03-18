722a9eeb0269d418c565631e83dded518dda8b3b
Dave Corley <smokydave@icloud.com>
2025-03-18 15:14:17 -0700

    CLEANUP: Make the camera closer to the basket

--- I Am Basket/Scripts/s3/basket/basket_p.lua
+++ I Am Basket/Scripts/s3/basket/basket_p.lua
@@ -98,7 +98,7 @@ end
 
 BasketFuncs.handleCameraMove = function(moveThisFrame)
 	local playerRotZ, _, _ = self.rotation:getAnglesZYX()
-	local cameraOffset = util.transform.rotateZ(playerRotZ):apply(util.vector3(0, -128, 128))
+	local cameraOffset = util.transform.rotateZ(playerRotZ):apply(util.vector3(0, -64, 48))
 	local newCameraPos = self.position + moveThisFrame + cameraOffset
 	camera.setStaticPosition(newCameraPos)
 	camera.setYaw(playerRotZ)
