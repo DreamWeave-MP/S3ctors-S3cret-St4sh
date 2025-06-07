f7a60fe18d7abac313f5b5923ff8125edc03fb94
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-06-07 03:15:56 -0700

    FEAT: Handle CPP and MPP downlaods in the soupault script as an optional argument

--- web/build.sh
+++ web/build.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env sh
-set -euo pipefail
+set -euo
 
 #
 # This script builds the project website. It downloads soupault as needed and
 # then runs it, the built website can be found at: web/build
 #
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --with-downloads)
+      DO_DOWNLOADS=1
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+if [ "${DO_DOWNLOADS:-0}" = 1 ]; then
+  # These used to be part of pkg.sh, but now pkg.sh is deprecated...
+  wget 'https://gitlab.com/modding-openmw/starwind-builder/-/jobs/artifacts/master/raw/Starwind_Community_Patch_Project.zip?job=build_cpp' -O sw_cpp.zip
+  unzip -o sw_cpp.zip -d sw_cpp/
+  rm sw_cpp.zip
+
+  wget 'https://gitlab.com/modding-openmw/Starwind-Builder/-/jobs/artifacts/master/download?job=build_tsi' -O sw_merged/Starwind.omwaddon
+  wget 'https://gitlab.com/modding-openmw/Starwind-Builder/-/jobs/artifacts/master/download?job=build_vanilla' -O sw_merged/Starwind_SP.omwaddon
+fi
 
 this_dir=$(realpath "$(dirname "${0}")")
 cd "${this_dir}"
