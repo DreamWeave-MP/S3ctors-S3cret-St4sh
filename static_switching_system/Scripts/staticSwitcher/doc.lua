7e138702b9e60086ede2b65ec43cf8e7f0ed196d
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-08-16 09:09:50 -0700

    FEAT: Add type annotations for raw and loaded modules

--- static_switching_system/Scripts/staticSwitcher/doc.lua
+++ static_switching_system/Scripts/staticSwitcher/doc.lua
@@ -35,9 +35,11 @@
 ---@field model string? mesh displayed for the object in game. If not present (usually a light), then the record is completely ignored.
 
 ---@class GameCell
----@field name string?
+---@field name string
 ---@field gridX integer exterior X coordinate of the cell. Be warned this is present even for interiors and fake exteriors.
 ---@field gridY integer exterior Y coordinate of the cell. Be warned this is present even for interiors and fake exteriors.
+---@field isExterior boolean whether or not the cell is a true exterior
+---@field id string
 
 ---@class GameObject
 ---@field recordId RecordId
@@ -46,7 +48,7 @@
 ---@field enabled boolean
 ---@field count integer
 ---@field cell GameCell
----@field isValid fun(): boolean whether or not the object is currently valid, eg teleporting or similar
+---@field isValid fun(self: GameObject): boolean whether or not the object is currently valid, eg teleporting or similar
 ---@field remove fun(self: GameObject, count: integer?) destroy the object completely
 ---@field position util.vector3
 ---@field getBoundingBox fun(): userdata
@@ -54,3 +56,19 @@
 ---@field sendEvent fun(eventName: string, eventData: any)
 ---@field id string The unique identifier for the object.
 ---@field rotation integer Totally not an integer and totally not updating these docs lol
+
+---@class SSSModule
+---@field cellNameMatches string[] list of cell names which will be fuzzy-matched for a given module
+---@field meshMap ReplacementMap
+---@field gridIndices table<SzudzikCoord, true>
+---@field logString string? prefix displayed when
+---@field ignoreRecords table<string, true> list of records which this module will explicitly ignore during replacement
+
+--- A Static Switching System module as it exists in yaml format.
+--- Most fields are NOT optional, and a corresponding JsonSchema exists for them as well.
+---@class SSSModuleRaw
+---@field log_name string?
+---@field replace_names string[] array of cell names to match replacements for
+---@field exterior_cells ExteriorGrid[] array of grid indices in which a particular module will replace objects
+---@field replace_meshes table<string, string> map of old meshes to new ones
+---@field ignore_records string[] records to ignore when replacing with this module. Typically used for scripted objects, but maybe not.
