e4f7dd3fd9bd3e28589114d3c5db233ec772a989
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-08-16 21:40:11 -0700

    CLEANUP: Migrate strings into their own file

---
+++ static_switching_system/Scripts/staticSwitcher/staticStrings.lua
@@ -0,0 +1,11 @@
+return {
+    PREFIX_FRAME = '[ %s ]:',
+    LOG_PREFIX = 'StaticSwitchingSystem',
+    LOG_FORMAT_STR = '%s %s',
+    MISSING_MESH_ERROR =
+    [[Requested model %s to replace %s on object %s, but the mesh was not found. The module: %s was not properly installed!]],
+    InvalidModuleNameStr = 'Invalid module name provided: %s. Either it does not exist, or has not replaced anything.',
+    ReplacingObjectsStr = 'Replacing Objects in cell: %s',
+    ReplacingIndividualObjectStr = 'Replacing object %s with model %s provided by module %s',
+    InvalidTypeStr = 'Invalid type was provided: %s',
+}
