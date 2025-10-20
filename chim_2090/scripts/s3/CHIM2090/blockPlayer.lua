b70c33cd03f2982aa8cd1ed4869e023159d58ae4
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-10-20 08:11:02 -0500

    FEAT: Initial implementation of parry system using onHit

--- chim_2090/scripts/s3/CHIM2090/blockPlayer.lua
+++ chim_2090/scripts/s3/CHIM2090/blockPlayer.lua
@@ -10,7 +10,6 @@ local self = require 'openmw.self'
 local time = require 'openmw_aux.time'
 local types = require 'openmw.types'
 local util = require 'openmw.util'
-local ui = require 'openmw.ui'
 
 local BLOCK_ANIM = 'activeblock'
 local HUGE = math.huge
@@ -23,9 +22,9 @@ local stopFns = {}
 
 -- Attributes
 local Attributes = self.type.stats.attributes
-local Strength = Attributes.strength(self)
 local Agility = Attributes.agility(self)
 local Luck = Attributes.luck(self)
+local Strength = Attributes.strength(self)
 
 -- Skills
 local Skills = self.type.stats.skills
@@ -51,7 +50,6 @@ local prevHealth = 0
 -- Values for timed block bonus
 -- Right now it uses lightning shield but maybe could be better?
 local hasTimedBonus = false
-local LightningShieldStrength = 0
 local BlockButton = 3
 
 local weaponTypes = types.Weapon.TYPE
@@ -61,6 +59,72 @@ local oneHandedTypes = {
     [weaponTypes.LongBladeOneHand] = true,
     [weaponTypes.BluntOneHand] = true,
 }
+
+---@class ParryManager
+local Parry = {
+    BASE_PARRY_FRAMES = 4,
+    BASE_SHIELD_FRAMES = 8,
+    BLOCK_FRAME_MULT = 3,
+    FRAME_DURATION = 1.0 / 60.0,
+    MIN_PARRY_FRAMES = 2,
+    MAX_PARRY_FRAMES = 16,
+    PER_UNIT_SHIELD_WEIGHT_PENALTY = 3,
+    remainingTime = 0,
+}
+
+function Parry.timeRemaining()
+    return Parry.remainingTime
+end
+
+function Parry.calculateParryFrames()
+    local blockSkill = math.min(Block.modified, 100) / 100            -- 0.0 to 1.0
+    local agilityBonus = (math.min(Agility.modified, 100) - 50) / 200 -- -0.25 to +0.25
+    local luckInfluence = (math.random(10) - 5) / 50                  -- -0.1 to +0.1 variance
+
+    local shield = self.type.getEquipment(self, self.type.EQUIPMENT_SLOT.CarriedLeft)
+    local shieldWeight = types.Armor.records[shield.recordId].weight
+
+    -- INVERSE relationship: lighter shields = more parry frames
+    -- Heaviest shields get minimal frames, lightest get bonus frames
+    local shieldFrames = math.max(0,
+        Parry.BASE_SHIELD_FRAMES -
+        math.floor(shieldWeight / Parry.PER_UNIT_SHIELD_WEIGHT_PENALTY)
+    )
+
+    local totalFrames = Parry.BASE_PARRY_FRAMES
+        + (blockSkill * Parry.BLOCK_FRAME_MULT) -- Skill adds 0-3 frames
+        + agilityBonus                          -- Agility adds ±2.5 frames
+        + luckInfluence                         -- Small random factor ±0.1 frames
+        + shieldFrames                          -- Equipment-based frames (inverse to weight)
+
+    return util.clamp(
+        math.floor(totalFrames + .5),
+        Parry.MIN_PARRY_FRAMES,
+        Parry.MAX_PARRY_FRAMES
+    )
+end
+
+function Parry.calculateParryFrameSeconds()
+    return Parry.calculateParryFrames() * Parry.FRAME_DURATION
+end
+
+function Parry.tick(dt)
+    if Parry.remainingTime <= 0 then return end
+    Parry.remainingTime = math.max(0.0, Parry.remainingTime - dt)
+end
+
+function Parry.start()
+    Parry.remainingTime = Parry.calculateParryFrameSeconds()
+end
+
+---@return boolean parryReady whether or not a parry can currently happen
+function Parry.ready()
+    return Parry.remainingTime > 0
+end
+
+function Parry.getDamage()
+
+end
 
 local function isBlocking()
     return anim.getActiveGroup(self, anim.BONE_GROUP.LeftArm) == BLOCK_ANIM
@@ -87,6 +151,81 @@ end
 
 local function playingHitstun()
     return anim.isPlaying(self, 'hitstun')
+end
+
+local blockSounds = {
+    lightarmor = 'light armor hit',
+    mediumarmor = 'medium armor hit',
+    heavyarmor = 'heavy armor hit',
+}
+local function playBlockSound()
+    local shield = self.type.getEquipment(self, self.type.EQUIPMENT_SLOT.CarriedLeft)
+    local shieldRecord = types.Armor.records[shield.recordId]
+    if not shieldRecord then return false end
+
+    local shieldSkill = I.Combat.getArmorSkill(shield)
+    local shieldSound = blockSounds[shieldSkill]
+    core.sound.playSound3d(shieldSound, self)
+end
+
+local function getShield()
+    return self.type.getEquipment(self, self.type.EQUIPMENT_SLOT.CarriedLeft)
+end
+
+local function handleBlockHit()
+    --- We also need to handle skill progression and degradation here!
+    playBlockSound()
+end
+
+---@class ParryDamageConfig
+local ParryDamageConfig = {
+    BASE_DAMAGE_MULTIPLIER = 0.5,
+    SHIELD_SIZE_INFLUENCE = 0.3,   -- How much shield size affects damage
+    STRENGTH_DAMAGE_BONUS = 0.002, -- 0.2% damage per strength point
+    BLOCK_SKILL_BONUS = 0.001,     -- 0.1% damage per block skill
+    MIN_DAMAGE_MULTIPLIER = 0.25,
+    MAX_DAMAGE_MULTIPLIER = 1.5,
+}
+
+---@param incomingDamage number? optional health damage caused by the incoming strike
+local function handleParryHit(incomingDamage)
+    handleBlockHit()
+
+    if not incomingDamage or incomingDamage <= 0 then
+        return 0
+    end
+
+    -- Calculate base multiplier with attributes
+    local damageMult = ParryDamageConfig.BASE_DAMAGE_MULTIPLIER
+
+    -- Strength bonus (stronger characters hit harder on parry)
+    local strengthBonus = Strength.modified * ParryDamageConfig.STRENGTH_DAMAGE_BONUS
+    damageMult = damageMult + strengthBonus
+
+    -- Block skill bonus (skilled blockers know how to redirect force better)
+    local blockBonus = Block.modified * ParryDamageConfig.BLOCK_SKILL_BONUS
+    damageMult = damageMult + blockBonus
+
+    -- Shield size influence (larger shields can redirect more force)
+    local shieldBounds = (getShield():getBoundingBox().halfSize.xy * 2):length()
+    local shieldBonus = shieldBounds * ParryDamageConfig.SHIELD_SIZE_INFLUENCE
+    damageMult = damageMult + shieldBonus
+
+    -- Clamp to reasonable bounds
+    damageMult = util.clamp(damageMult, ParryDamageConfig.MIN_DAMAGE_MULTIPLIER, ParryDamageConfig.MAX_DAMAGE_MULTIPLIER)
+
+    -- Convert health damage to fatigue damage
+    local outgoingDamage = incomingDamage * damageMult
+
+    print(
+        ("Parry: %.1f health damage -> %.1f fatigue damage (mult: %.2f)"):format(
+            incomingDamage,
+            outgoingDamage,
+            damageMult
+        )
+    )
+
+    return outgoingDamage
 end
 
 local function usingOneHanded()
@@ -137,6 +276,8 @@ local blockSpeedConfig = {
     LuckWeight = 0.15,
     AgilityWeight = 0.3,
     BaseSpeed = 0.75,
+    ShieldWeightPenalty = 0.005,
+    MaxShieldWeightSlow = 0.5,
 }
 
 --- Determine how fast the block animation should play, based on a combinatino of luck, agility, and randomness.
@@ -146,14 +287,21 @@ local function getBlockSpeed()
     local normalizedAgility = math.min(Agility.modified, 100) / 100.0
     local normalizedLuck = math.min(Luck.modified, 100) / 100.0
 
-    local randomFactor = (math.random(25) - 5) / 100.0
+    local randomFactor = (math.random(5) - 5) / 100.0
 
     local speedModifier = (normalizedLuck * blockSpeedConfig.LuckWeight)
         + (normalizedAgility * blockSpeedConfig.AgilityWeight)
         + randomFactor
 
-    return blockSpeedConfig.BaseSpeed
-        + (speedModifier * normalizedFatigue())
+    local shield = self.type.getEquipment(self, self.type.EQUIPMENT_SLOT.CarriedLeft)
+    local shieldWeight = shield and types.Armor.records[shield.recordId].weight or 0
+    local weightPenalty = math.min(
+        shieldWeight * blockSpeedConfig.ShieldWeightPenalty,
+        blockSpeedConfig.MaxShieldWeightSlow
+    )
+
+    return (blockSpeedConfig.BaseSpeed - weightPenalty)
+        + speedModifier * normalizedFatigue()
 end
 
 local blockAnimData = {
@@ -185,50 +333,10 @@ local function playIdleAnimation()
     I.AnimationController.playBlendedAnimation('idle1', idleAnimData)
 end
 
-local function disableTimedBlockBonus()
-    if not hasTimedBonus then return end
-
-    ActiveEffects:modify(-LightningShieldStrength, core.magic.EFFECT_TYPE.LightningShield)
-    print('Updated lightning shield value:', ActiveEffects:getEffect(core.magic.EFFECT_TYPE.LightningShield).magnitude)
-    -- ui.showMessage("Removing lightning shield")
-    hasTimedBonus = false
-end
-
-local function updateTimedBlockDamage()
-    local itemWeight = getBlockingWeight()
-
-    -- Base damage calculation
-    local baseDamage = (Block.base * 0.2) + (Strength.modified * 0.1) + (itemWeight * .1)
-
-    -- The shield does 10% magnitude as damage, so multiply intended damage by 10
-    LightningShieldStrength = math.min(baseDamage, 50) * 10
-end
-
-local function applyTimedBlockBonus()
-    updateTimedBlockDamage()
-
-    ActiveEffects:modify(LightningShieldStrength, core.magic.EFFECT_TYPE.LightningShield)
-    print('Updated lightning shield value:', LightningShieldStrength)
-
-    -- ui.showMessage("Adding lightning shield")
-
-    hasTimedBonus = true
-end
-
 local function timedBlockHandler(group, key)
-    -- print(group, key)
-
-    if key == 'stop' then
-        -- ui.showMessage("Disabling timed block bonus from handler")
-        applyTimedBlockBonus()
-
-        -- disableTimedBlockBonus()
-
-        -- we need some way to calculate the delay!
-        async:newUnsavableSimulationTimer(time.second / 2, disableTimedBlockBonus)
-        -- playIdleAnimation()
+    if key == 'half' then
+        Parry.start()
     end
-    -- print(groupName, key)
 end
 
 I.AnimationController.addTextKeyHandler(BLOCK_ANIM, timedBlockHandler)
@@ -236,12 +344,7 @@ I.AnimationController.addTextKeyHandler(BLOCK_ANIM, timedBlockHandler)
 local function toggleBlock(enable)
     if isBlocking() == enable then return end
 
-    if enable then
-        playBlockAnimation()
-    else
-        disableTimedBlockBonus()
-        playIdleAnimation()
-    end
+    (enable and playBlockAnimation or playIdleAnimation)()
 end
 
 local function blockBegin(button)
@@ -257,7 +360,6 @@ end
 local function beginIFrames(currentLegs)
     if stopFns["iframes"] then return end
 
-    ui.showMessage('Adding iFrames')
     ActiveEffects:modify(SanctuaryStrength, core.magic.EFFECT_TYPE.Sanctuary)
     hasIFrames = true
 
@@ -267,7 +369,6 @@ local function beginIFrames(currentLegs)
                 if not stopFns["iframes"] then return end
 
                 if hasIFrames then
-                    ui.showMessage("Removing iFrames")
                     ActiveEffects:modify(-SanctuaryStrength, core.magic.EFFECT_TYPE.Sanctuary)
                     hasIFrames = false
                 end
@@ -309,13 +410,21 @@ local function interruptBlock()
 end
 
 --- Implement logic for determining the parry window, and associated fatigue damage
---- Also consider implementing iFrames. The current mechanic appears to be designed so as to give you a shitton of sanctuary effect 
+--- Also consider implementing iFrames. The current mechanic appears to be designed so as to give you a shitton of sanctuary effect
 --- until the hit animation finishes.
 return {
     interfaceName = 's3ChimBlock',
     interface = {
+        calculateParryFrames = Parry.calculateParryFrames,
+        calculateParryFrameSeconds = Parry.calculateParryFrameSeconds,
         canBlock = canBlock,
+        getBlockSpeed = getBlockSpeed,
+        handleBlockHit = handleBlockHit,
+        handleParryHit = handleParryHit,
         isBlocking = isBlocking,
+        parryReady = Parry.ready,
+        parryTimeRemaining = Parry.timeRemaining,
+        playBlockSound = playBlockSound,
         toggleBlock = toggleBlock,
     },
     engineHandlers = {
@@ -342,7 +451,7 @@ return {
                 if not hasTimedBonus then
                     beginIFrames(currentLegs)
 
-                    ui.showMessage("Blocking hit")
+                    -- ui.showMessage("Blocking hit")
                 else
                     -- CurrentParryDelay = MaxParryDelay
                     -- ui.showMessage("Parry!")
@@ -350,6 +459,7 @@ return {
             end
 
             prevHealth = health.current
+            Parry.tick(dt)
         end,
     }
 }
