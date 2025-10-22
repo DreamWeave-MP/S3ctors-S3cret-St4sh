0271d38167f86b395db018d2b631f47f83e3626e
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-10-22 15:40:40 -0500

    CLEANUP: Reformat settings pages and migrate fatigue GMSTs into dynamicManager

--- chim_2090/scripts/s3/CHIM2090/settingsPage.lua
+++ chim_2090/scripts/s3/CHIM2090/settingsPage.lua
@@ -1,22 +1,29 @@
-local modInfo = require('scripts.s3.CHIM2090.modInfo')
-local I = require('openmw.interfaces')
+local modInfo = require 'scripts.s3.CHIM2090.modInfo'
+local I = require 'openmw.interfaces'
 
 I.Settings.registerPage {
   key = modInfo.name,
   l10n = modInfo.l10nName,
-  name = "CHIM 2090",
-  description = "Manages actor fatigue, carry weight, hit chance, and strength in combat."
+  name = 'CHIM 2090 - Damage, Crit, Fumble',
+  description = 'Manages actor fatigue, carry weight, hit chance, and strength in combat.'
 }
 
 I.Settings.registerPage {
   key = modInfo.name .. 'Block & Parry',
   l10n = modInfo.l10nName,
-  name = "CHIM 2090 - Block & Parry",
-  description = "Manages blocking and parrying effectiveness and formulae."
+  name = 'CHIM 2090 - Block & Parry',
+  description = 'Manages blocking and parrying effectiveness and formulae.'
+}
+
+I.Settings.registerPage {
+  key = modInfo.name .. 'Dynamic Stats',
+  l10n = modInfo.l10nName,
+  name = 'CHIM 2090 - Dynamic Stats',
+  description = 'DynamicStatPageDesc',
 }
 
 print(
-  ("%s loaded version %s. Thank you for playing %s! <3"):format(
+  ('%s loaded version %s. Thank you for playing %s! <3'):format(
     modInfo.logPrefix,
     modInfo.version,
     modInfo.name)
