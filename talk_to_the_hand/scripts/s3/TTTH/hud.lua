fd4580e8efc3aaa54ebec414cd36beb8de5ff99c
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-11-12 13:43:58 -0600

    FEAT: Initial implementation of H4ND

---
+++ talk_to_the_hand/scripts/s3/TTTH/hud.lua
@@ -0,0 +1,409 @@
+local async = require 'openmw.async'
+local core = require 'openmw.core'
+local storage = require 'openmw.storage'
+local ui = require 'openmw.ui'
+local util = require 'openmw.util'
+
+local I = require 'openmw.interfaces'
+local s3lf = I.s3lf
+
+local HudCore,
+CastableIndicator,
+WeaponIndicator,
+EffectBar,
+ThumbAtlas,
+MiddleAtlas,
+PinkyAtlas,
+TotalDelay,
+CurrentDelay
+
+local H4ndStorage = storage.playerSection('SettingsTalkToTheHandMain')
+---@class H4ND: ProtectedTable
+---@field UIFramerate integer
+---@field HUDWidth number
+---@field HUDPos util.vector2
+---@field ThumbStat string
+---@field PinkyStat string
+---@field MiddleStat string
+---@field fatigueColor util.color
+---@field magickaColor util.color
+---@field healthColor util.color
+---@field DurabilityColor util.color
+---@field CastChanceColor util.color
+local H4ND = I.S3ProtectedTable.new {
+    storageSection = H4ndStorage,
+    logPrefix = '[H4ND]:',
+    subscribeHandler = false,
+}
+
+local function getSelectedWeaponIcon()
+    local weapon = s3lf.getEquipment(s3lf.EQUIPMENT_SLOT.CarriedRight)
+    if not weapon then
+        return core.stats.Skill.records.handtohand.icon
+    end
+
+    return weapon.type.records[weapon.recordId].icon
+end
+
+---@param statName string name of a dynamic stat
+local function getStatFrame(statName)
+    local stat = I.s3lf[statName]
+    local normalized = util.clamp(stat.current / stat.base, 0.0, 1.0)
+    local difference = 1.0 - normalized
+    return math.max(1, math.floor(difference * 100))
+end
+
+---@param atlasName string
+local function getAtlasTargetTile(atlasName)
+    local statName = H4ND[atlasName .. 'Stat']
+    return getStatFrame(statName)
+end
+
+---@param elementName string
+local function getColorForElement(elementName)
+    local settingName = elementName .. 'Stat'
+    local stat = H4ND[settingName]
+
+    return H4ND[stat .. 'Color']
+end
+
+---@param atlasName string
+---@param atlas ImageAtlas
+local function updateAtlas(atlasName, atlas)
+    local targetTile = getAtlasTargetTile(atlasName)
+    if atlas.currentTile == targetTile then return end
+
+    atlas:cycleFrame(atlas.currentTile < targetTile)
+end
+
+local screenSize = ui.screenSize()
+
+local Vectors = {
+    BottomLeft = util.vector2(0, 1),
+    Center = util.vector2(.5, .5),
+    LeftCenter = util.vector2(0, .5),
+    RelativeHandSize = util.vector2(H4ND.HUDWidth, H4ND.HUDWidth * 2),
+    Zero = util.vector2(0, 0),
+    Tiles = {
+        Pinky = util.vector2(128, 256),
+        Middle = util.vector2(256, 512),
+        Thumb = util.vector2(278, 271),
+    },
+    TopRight = util.vector2(1, 0),
+}
+
+local handSize = screenSize:emul(Vectors.RelativeHandSize)
+
+--- Table mapping X pixel length to Y pixel length
+local Ratio = {
+    UL = 1.3475177305,
+    UR = 1.37795275591,
+    Pinky = 2.0,
+    Middle = 1.37,
+    Thumb = 0.974820143885,
+}
+
+local Width = {
+    UL = 1 / 6,
+    Pinky = function()
+        return handSize.x * .275
+    end,
+    Middle = function()
+        return handSize.x * .55
+    end,
+    Thumb = function()
+        return handSize.x * .625
+    end,
+}
+
+local Attrs = {
+    ChanceBar = function()
+        return util.vector2(handSize.x * .2, handSize.x * 0.02)
+    end,
+    EffectBar = function()
+        return util.vector2(handSize.x, handSize.y * 0.1)
+    end,
+    Middle = function()
+        local width = Width.Middle()
+        return util.vector2(width, width * Ratio.Middle), util.vector2(handSize.x * .255, handSize.y * 0.015)
+    end,
+    Pinky = function()
+        local width = Width.Pinky()
+        return util.vector2(width, width * Ratio.Pinky), util.vector2(handSize.x * .64, handSize.y * .235)
+    end,
+    SubIcon = function()
+        return util.vector2(handSize.x * .2, handSize.x * .2)
+    end,
+    Thumb = function()
+        local width = Width.Thumb()
+        return util.vector2(width, width * Ratio.Thumb), util.vector2(handSize.x * .075, handSize.y * .35)
+    end,
+    Weapon = function()
+        return util.vector2(handSize.x * .025, handSize.y * .15)
+    end,
+}
+
+PinkyAtlas = I.S3AtlasConstructor.constructAtlas {
+    tileSize = Vectors.Tiles.Pinky,
+    tilesPerRow = 10,
+    totalTiles = 100,
+    atlasPath = 'textures/s3/chim/tribunalpinky.dds'
+}
+
+MiddleAtlas = I.S3AtlasConstructor.constructAtlas {
+    tileSize = Vectors.Tiles.Middle,
+    tilesPerRow = 10,
+    totalTiles = 100,
+    atlasPath = 'textures/s3/chim/tribunalmiddle.dds'
+}
+
+ThumbAtlas = I.S3AtlasConstructor.constructAtlas {
+    tileSize = Vectors.Tiles.Thumb,
+    tilesPerRow = 10,
+    totalTiles = 100,
+    atlasPath = 'textures/s3/chim/tribunalthumb.dds'
+}
+
+local ThumbSize, ThumbPos = Attrs.Thumb()
+ThumbAtlas:spawn {
+    color = getColorForElement('Thumb'),
+    name = 'Thumb',
+    size = ThumbSize,
+    position = ThumbPos,
+}
+
+local middleSize, middlePos = Attrs.Middle()
+MiddleAtlas:spawn {
+    color = getColorForElement('Middle'),
+    name = 'Middle',
+    size = middleSize,
+    position = middlePos,
+}
+
+local pinkySize, pinkyPos = Attrs.Pinky()
+PinkyAtlas:spawn {
+    color = getColorForElement('Pinky'),
+    name = 'Pinky',
+    size = pinkySize,
+    position = pinkyPos,
+}
+
+CastableIndicator = ui.create {
+    type = ui.TYPE.Flex,
+    name = 'Castable',
+    layer = 'HUD',
+    props = {
+        anchor = Vectors.TopRight,
+        relativePosition = Vectors.TopRight + util.vector2(-.02, .03),
+    },
+    content = ui.content {
+        {
+            type = ui.TYPE.Image,
+            name = 'CastableIcon',
+            props = {
+                resource = ui.texture { path = getSelectedWeaponIcon() },
+                size = Attrs.SubIcon(),
+            }
+        },
+        {
+            type = ui.TYPE.Image,
+            name = 'CastChanceBar',
+            props = {
+                resource = ui.texture { path = 'white' },
+                size = Attrs.ChanceBar(),
+                color = H4ND.CastChanceColor,
+            },
+        },
+    }
+}
+
+WeaponIndicator = ui.create {
+    type = ui.TYPE.Flex,
+    layer = 'HUD',
+    name = 'Weapon',
+    props = {
+        position = Attrs.Weapon(),
+    },
+    content = ui.content {
+        {
+            type = ui.TYPE.Image,
+            name = 'WeaponIcon',
+            props = {
+                resource = ui.texture { path = getSelectedWeaponIcon() },
+                size = Attrs.SubIcon(),
+            }
+        },
+        {
+            type = ui.TYPE.Image,
+            name = 'DurabilityBar',
+            props = {
+                resource = ui.texture { path = 'white' },
+                size = Attrs.ChanceBar(),
+                color = H4ND.DurabilityColor,
+            },
+        },
+    }
+}
+
+EffectBar = ui.create {
+    type = ui.TYPE.Image,
+    layer = 'HUD',
+    props = {
+        resource = ui.texture { path = 'white', },
+        size = Attrs.EffectBar(),
+        color = util.color.hex('ff0abb'),
+        anchor = Vectors.BottomLeft,
+        relativePosition = Vectors.BottomLeft,
+    }
+}
+
+HudCore = ui.create {
+    layer = 'HUD',
+    name = 'H4ND',
+    props = {
+        size = handSize,
+        anchor = Vectors.BottomLeft,
+        relativePosition = H4ND.HUDPos,
+    },
+    content = ui.content {
+        ThumbAtlas.element,
+        MiddleAtlas.element,
+        PinkyAtlas.element,
+        WeaponIndicator,
+        EffectBar,
+        CastableIndicator,
+    }
+}
+
+local function updateStatFrames()
+    if not HudCore then return end
+
+    for elementName, atlas in pairs {
+        Thumb = ThumbAtlas,
+        Middle = MiddleAtlas,
+        Pinky = PinkyAtlas,
+    } do
+        updateAtlas(elementName, atlas)
+    end
+end
+
+local namesToAtlases, statsToAtlases = {
+    Middle = MiddleAtlas,
+    Pinky = PinkyAtlas,
+    Thumb = ThumbAtlas,
+}, {
+    MiddleStat = MiddleAtlas,
+    PinkyStat = PinkyAtlas,
+    ThumbStat = ThumbAtlas
+}
+
+H4ndStorage:subscribe(
+    async:callback(
+        function(_, key)
+            local atlasName, atlas
+            if key == 'ThumbColor' or key == 'ThumbStat' then
+                atlasName, atlas = 'Thumb', ThumbAtlas
+            elseif key == 'MiddleColor' or key == 'MiddleStat' then
+                atlasName, atlas = 'Middle', MiddleAtlas
+            elseif key == 'PinkyColor' or key == 'PinkyStat' then
+                atlasName, atlas = 'Pinky', PinkyAtlas
+            elseif key == 'HUDPos' or key == 'HUDWidth' then
+                if key == 'HUDWidth' then
+                    local width = H4ndStorage:get('HUDWidth')
+                    local relativeSize = util.vector2(width, width * 2)
+
+                    handSize = ui.screenSize():emul(relativeSize)
+                    HudCore.layout.props.size = handSize
+
+                    local ThumbProps = ThumbAtlas.element.layout.props
+                    ThumbProps.size, ThumbProps.position = Attrs.Thumb()
+                    ThumbAtlas.element:update()
+
+                    local MiddleProps = MiddleAtlas.element.layout.props
+                    MiddleProps.size, MiddleProps.position = Attrs.Middle()
+                    MiddleAtlas.element:update()
+
+                    local PinkyProps = PinkyAtlas.element.layout.props
+                    PinkyProps.size, PinkyProps.position = Attrs.Pinky()
+                    PinkyAtlas.element:update()
+
+                    EffectBar.layout.props.size = Attrs.EffectBar()
+                    EffectBar:update()
+                elseif key == 'HUDPos' then
+                    HudCore.layout.props.relativePosition = H4ndStorage:get('HUDPos')
+                end
+
+                HudCore:update()
+            elseif key == 'UIFramerate' then
+                TotalDelay = 1 / H4ndStorage:get('UIFramerate')
+            end
+
+            if atlasName and atlas then
+                atlas.element.layout.props.color = getColorForElement(atlasName)
+                atlas.element:update()
+
+                if key == 'PinkyStat' or key == 'MiddleStat' or key == 'ThumbStat' then
+                    local usedAttributes, statToUpdate = {
+                        magicka = true,
+                        health = true,
+                        fatigue = true,
+                    }, nil
+
+                    local thisAtlas = statsToAtlases[key]
+
+                    local invalidStat = H4ndStorage:get(key)
+
+                    for elementName, statName in pairs {
+                        Pinky = H4ND.PinkyStat,
+                        Thumb = H4ND.ThumbStat,
+                        Middle = H4ND.MiddleStat,
+                    } do
+                        usedAttributes[statName] = nil
+
+                        if statName == invalidStat and namesToAtlases[elementName] ~= thisAtlas then
+                            atlasName = elementName
+                        end
+                    end
+
+                    statToUpdate = next(usedAttributes)
+
+                    if not statToUpdate then return end
+
+                    s3lf.gameObject:sendEvent('H4NDCorrectSecondAttribute', {
+                        stat = statToUpdate,
+                        atlasName = atlasName,
+                    })
+                end
+            end
+        end
+    )
+)
+
+CurrentDelay, TotalDelay = 0, 1 / H4ND.UIFramerate
+return {
+    interfaceName = 'H4nd',
+    interface = {
+        HUD = function()
+            return HudCore
+        end,
+    },
+    eventHandlers = {
+        H4NDCorrectSecondAttribute = function(atlasData)
+            local stat, atlasName = atlasData.stat, atlasData.atlasName
+            H4ND[atlasName .. 'Stat'] = stat
+            namesToAtlases[atlasName].element:update()
+        end
+    },
+    engineHandlers = {
+        onFrame = function(dt)
+            if CurrentDelay < TotalDelay then
+                CurrentDelay = CurrentDelay + dt
+                return
+            else
+                CurrentDelay = 0
+            end
+
+            updateStatFrames()
+        end,
+    }
+}
---
+++ talk_to_the_hand/scripts/s3/TTTH/menu.lua
@@ -0,0 +1,148 @@
+local core = require 'openmw.core'
+local util = require 'openmw.util'
+
+local I = require 'openmw.interfaces'
+
+local ModName = 'TalkToTheHand'
+
+local statNames = { 'fatigue', 'magicka', 'health' }
+
+---@param key string
+---@param renderer DefaultSettingRenderer
+---@param argument SettingRendererOptions
+---@param name string
+---@param description string
+---@param default string|number|boolean
+local function setting(key, renderer, argument, name, description, default)
+    return {
+        key = key,
+        renderer = renderer,
+        argument = argument,
+        name = name,
+        description = description,
+        default = default,
+    }
+end
+
+local function colorFromGMST(gmst)
+    local colorString = core.getGMST(gmst)
+    local numberTable = {}
+    for numberString in colorString:gmatch("([^,]+)") do
+        if #numberTable == 3 then break end
+        local number = tonumber(numberString:match("^%s*(.-)%s*$"))
+        if number then
+            table.insert(numberTable, number / 255)
+        end
+    end
+
+    if #numberTable < 3 then error('Invalid color GMST name: ' .. gmst) end
+
+    return util.color.rgb(table.unpack(numberTable))
+end
+
+I.Settings.registerPage {
+    key = ModName,
+    l10n = ModName,
+    name = 'HandPageName',
+    description = 'HandPageDesc',
+}
+
+I.Settings.registerGroup {
+    key = 'Settings' .. ModName .. 'Main',
+    page = ModName,
+    order = 0,
+    l10n = ModName,
+    name = 'MainGroupName',
+    description = 'MainGroupDesc',
+    permanentStorage = true,
+    settings = {
+        setting(
+            'UIFramerate',
+            'number',
+            { integer = true, min = 1, max = 144 },
+            'UIFramerateName',
+            'UIFramerateDesc',
+            60
+        ),
+        setting(
+            'HUDWidth',
+            'number',
+            { integer = false, min = 0.01, max = 0.5 },
+            'HUDWidthName',
+            'HUDWidthDesc',
+            0.15
+        ),
+        setting(
+            'HUDPos',
+            'ScreenPosition',
+            {},
+            'HUDPosName',
+            'HUDPosDesc',
+            util.vector2(0, 1)
+        ),
+        setting(
+            'ThumbStat',
+            'select',
+            { l10n = ModName, items = statNames },
+            'ThumbStatName',
+            'ThumbStatDesc',
+            'fatigue'
+        ),
+        setting(
+            'MiddleStat',
+            'select',
+            { l10n = ModName, items = statNames },
+            'MiddleStatName',
+            'MiddleStatDesc',
+            'health'
+        ),
+        setting(
+            'PinkyStat',
+            'select',
+            { l10n = ModName, items = statNames },
+            'PinkyStatName',
+            'PinkyStatDesc',
+            'magicka'
+        ),
+        setting(
+            'magickaColor',
+            'color',
+            {},
+            'MagickaColorName',
+            'MagickaColorDesc',
+            colorFromGMST('FontColor_color_magic')
+        ),
+        setting(
+            'healthColor',
+            'color',
+            {},
+            'HealthColorName',
+            'HealthColorDesc',
+            colorFromGMST('FontColor_color_health')
+        ),
+        setting(
+            'fatigueColor',
+            'color',
+            {},
+            'FatigueColorName',
+            'FatigueColorDesc',
+            colorFromGMST('FontColor_color_fatigue')
+        ),
+        setting(
+            'DurabilityColor',
+            'color',
+            {},
+            'DurabilityColorName',
+            'DurabilityColorDesc',
+            colorFromGMST('FontColor_color_weapon_fill')
+        ),
+        setting(
+            'CastChanceColor',
+            'color',
+            {},
+            'CastChanceColorName',
+            'CastChanceColorDesc',
+            colorFromGMST('FontColor_color_magic_fill')
+        ),
+    }
+}
