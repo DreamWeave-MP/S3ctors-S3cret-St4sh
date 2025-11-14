58c1ca118fd52e5d88d2e16d24a4daf85211f56c
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-11-13 20:25:43 -0600

    CLEANUP: Migrate attribute functions into constants too

--- talk_to_the_hand/scripts/s3/TTTH/constants.lua
+++ talk_to_the_hand/scripts/s3/TTTH/constants.lua
@@ -1,7 +1,56 @@
 local util = require 'openmw.util'
 
+---@alias ScaleFunction fun(size: util.vector2): util.vector2
+
 ---@class H4NDConstants
+---@field Attrs table<string, ScaleFunction>
+---@field Colors table<string, util.color>
+---@field Vectors table<string, util.vector2|table<string, util.vector2>>
 return {
+    --- Size functions for hand images are aspect ratios
+    Attrs = {
+        ---@param handSize util.vector2
+        ---@return util.vector2
+        ChanceBar = function(handSize)
+            return util.vector2(handSize.x * .2, handSize.x * 0.02)
+        end,
+        ---@param handSize util.vector2
+        ---@return util.vector2
+        EffectBar = function(handSize)
+            return util.vector2(handSize.x, handSize.y * 0.1)
+        end,
+        ---@param handSize util.vector2
+        ---@return util.vector2, util.vector2
+        Middle = function(handSize)
+            local width = handSize.x * .55
+
+            return util.vector2(width, width * 1.37), util.vector2(handSize.x * .255, handSize.y * 0.015)
+        end,
+        ---@param handSize util.vector2
+        ---@return util.vector2, util.vector2
+        Pinky = function(handSize)
+            local width = handSize.x * .275
+
+            return util.vector2(width, width * 2.0), util.vector2(handSize.x * .64, handSize.y * .235)
+        end,
+        ---@param handSize util.vector2
+        ---@return util.vector2
+        SubIcon = function(handSize)
+            return util.vector2(handSize.x * .2, handSize.x * .2)
+        end,
+        ---@param handSize util.vector2
+        ---@return util.vector2, util.vector2
+        Thumb = function(handSize)
+            local width = handSize.x * .625
+
+            return util.vector2(width, width * 0.974820143885), util.vector2(handSize.x * .075, handSize.y * .35)
+        end,
+        ---@param handSize util.vector2
+        ---@return util.vector2
+        Weapon = function(handSize)
+            return util.vector2(handSize.x * .025, handSize.y * .15)
+        end,
+    },
     Colors = {
         Black = util.color.hex('000000'),
     },
