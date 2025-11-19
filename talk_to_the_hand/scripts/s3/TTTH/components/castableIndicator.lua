06189f5fc762f9e86fd9a536d40ddd64c93b942b
Dave Corley <corleycomputerrepair@protonmail.ch>
2025-11-19 08:55:13 -0600

    CLEANUP: Refactor castableIndicator and weaponIndicator to work with debug mode

--- talk_to_the_hand/scripts/s3/TTTH/components/castableIndicator.lua
+++ talk_to_the_hand/scripts/s3/TTTH/components/castableIndicator.lua
@@ -23,69 +23,95 @@ return function(constructor)
 
     local width = constructor.H4ND.CastableIndicatorSize
     return ui.create {
-        type = ui.TYPE.Flex,
         name = 'CastableIndicator',
         layer = 'Windows',
         props = {
-            autoSize = false,
             anchor = constructor.H4ND.CastableIndicatorAnchor,
             relativeSize = util.vector2(width, width * 1.125),
             relativePosition = constructor.H4ND.CastableIndicatorPos,
         },
         events = constructor.dragEvents,
         content = ui.content {
-            -- {
-            --     type = ui.TYPE.Image,
-            --     name = 'DebugContent',
-            --     props = {
-            --         resource = ui.texture { path = 'white', },
-            --         color = util.color.hex('00ffff'),
-            --         visible = constructor.useDebug,
-            --         relativeSize = Vectors.BottomRight,
-            --     },
-            -- },
             {
-                type = ui.TYPE.Image,
-                template = I.MWUI.templates.borders,
-                name = 'CastableIcon',
+                type = ui.TYPE.Flex,
+                name = 'CastableIndicator',
                 props = {
-                    resource = ui.texture { path = constructor.castableIcon or 'white' },
-                    relativeSize = Attrs.IndicatorSize(),
-                    visible = true,
-                    color = constructor.castableIcon == nil and Colors.Black or nil,
-                    alpha = constructor.castableIcon ~= nil and 1.0 or .5,
-                },
-                external = {
-                    grow = 1,
-                    stretch = 1,
-                }
-            },
-            {
-                name = 'CastChanceContainer',
-                template = I.MWUI.templates.borders,
-                props = {
-                    relativeSize = constructor.Constants.Attrs.BarContainer(),
+                    autoSize = false,
+                    relativeSize = Vectors.BottomRight,
                 },
                 content = ui.content {
                     {
                         type = ui.TYPE.Image,
-                        name = 'CastChanceBackground',
+                        template = I.MWUI.templates.borders,
+                        name = 'CastableIcon',
                         props = {
-                            resource = ui.texture { path = 'white' },
+                            resource = ui.texture { path = constructor.castableIcon or 'white' },
+                            relativeSize = Attrs.IndicatorSize(),
+                            visible = true,
+                            color = constructor.castableIcon == nil and Colors.Black or nil,
+                            alpha = constructor.castableIcon ~= nil and 1.0 or .5,
+                        },
+                        external = {
+                            grow = 1,
+                            stretch = 1,
+                        }
+                    },
+                    {
+                        name = 'CastChanceContainer',
+                        template = I.MWUI.templates.borders,
+                        props = {
+                            relativeSize = constructor.Constants.Attrs.BarContainer(),
+                        },
+                        content = ui.content {
+                            {
+                                type = ui.TYPE.Image,
+                                name = 'CastChanceBackground',
+                                props = {
+                                    resource = ui.texture { path = 'white' },
+                                    relativeSize = Vectors.BottomRight,
+                                    color = Colors.Black,
+                                    alpha = 0.5,
+                                },
+                            },
+                            {
+                                type = ui.TYPE.Image,
+                                name = 'CastChanceBar',
+                                props = {
+                                    resource = ui.texture { path = 'white' },
+                                    relativeSize = util.vector2(constructor.castableWidth, 1),
+                                    color = constructor.barColor,
+                                },
+                            },
+                        }
+                    },
+                }
+            },
+            {
+                name = 'DebugContent',
+                props = {
+                    visible = constructor.useDebug,
+                    relativeSize = Vectors.BottomRight,
+                },
+                content = ui.content {
+                    {
+                        type = ui.TYPE.Image,
+                        props = {
                             relativeSize = Vectors.BottomRight,
-                            color = Colors.Black,
-                            alpha = 0.5,
+                            alpha = .5,
+                            resource = ui.texture { path = 'white', },
+                            color = util.color.hex('00ffff'),
                         },
                     },
                     {
-                        type = ui.TYPE.Image,
-                        name = 'CastChanceBar',
+                        template = I.MWUI.templates.textHeader,
                         props = {
-                            resource = ui.texture { path = 'white' },
-                            relativeSize = util.vector2(constructor.castableWidth, 1),
-                            color = constructor.barColor,
+                            anchor = Vectors.Center,
+                            relativePosition = Vectors.Center,
+                            textAlignH = ui.ALIGNMENT.Center,
+                            textAlignV = ui.ALIGNMENT.Center,
+                            text = 'Castable Container',
                         },
-                    },
+                    }
                 }
             },
         }
