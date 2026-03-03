1373bbf723ece0c9c22b626f7cbd1945ff25573c
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-03 08:30:17 -0700

    FIX: OpenMW's require path uses `Data Files` as its root
    This will allow us to keep consistent `require` paths in shared code.

--- content/s3maphore/00 Core/MWSE/mods/s3maphore/main.lua
+++ content/s3maphore/00 Core/MWSE/mods/s3maphore/main.lua
@@ -1,5 +1,5 @@
 -- mwse.memory.writeNoOperation{address = 0x4BB666, length = 18}
-package.path = package.path .. ";.\\Data Files\\scripts\\?.lua;.\\Data Files\\doc\\?.lua"
+package.path = package.path .. ";.\\Data Files\\?.lua;"
 
 require("s3maphore.meta")
 require("s3maphore.mcm")
