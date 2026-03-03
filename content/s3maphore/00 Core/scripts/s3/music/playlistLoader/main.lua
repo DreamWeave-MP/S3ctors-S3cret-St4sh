f74292cf6d2a8582eca26a05063f95f4b6510dac
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-03 11:45:26 -0600

    CLEANUP: Migrate the rest of the coroutine loader into its own file, which returns the core loader function

---
+++ content/s3maphore/00 Core/scripts/s3/music/playlistLoader/main.lua
@@ -0,0 +1,69 @@
+local util = require 'openmw.util'
+local vfs = require 'openmw.vfs'
+
+local musicUtil = require 'scripts.s3.music.util'
+
+local PlaylistEnvironment = require 'scripts.s3.music.playlistLoader.environment'
+local Strings = require 'scripts.s3.music.staticStrings'
+
+local function playlistCoroutineLoader()
+  local result, codeString
+
+  for _, file in ipairs(PlaylistFileList) do
+    musicUtil.debugLog('reading playlist file', file)
+
+    --- open the file
+    local ok, fileHandle = pcall(vfs.open, file)
+    if not ok then goto fail end
+
+    --- read all lines
+    codeString = fileHandle:read('*a')
+
+    --- util.loadCode it
+    ok, result = pcall(util.loadCode, codeString, PlaylistEnvironment)
+    if not ok or type(result) ~= 'function' then goto fail end
+
+    --- call the resulting function to get the inner playlist array
+    ok, result = pcall(result)
+
+    if type(result) ~= 'table' then goto fail end
+    musicUtil.debugLog(musicUtil.deepToString(result, 3))
+
+    for _, playlist in ipairs(result) do
+      MusicManager.registerPlaylist(playlist)
+      coroutine.yield(playlist)
+    end
+
+    ::fail::
+    if not ok then
+      print(
+        Strings.FailedToLoadPlaylist:format(file, result)
+      )
+    else
+      fileHandle:close()
+    end
+  end
+end
+
+-- Create the coroutine
+local playlistLoaderCo = coroutine.create(playlistCoroutineLoader)
+local playlistCount = 0
+
+---@return boolean? canPlayback if true, loading has finished and playback can start
+return function()
+  local status = coroutine.status(playlistLoaderCo)
+  if status == 'dead' then return true end
+
+  local ok, playlist = coroutine.resume(playlistLoaderCo)
+
+  --- Explore, battle, special
+  if ok and playlist then
+    musicUtil.debugLog("Registered playlist:", playlist.id)
+    playlistCount = playlistCount + 1
+  elseif coroutine.status(playlistLoaderCo) == "dead" then
+    print(
+      Strings.InitializationFinished:format(playlistCount)
+    )
+    return true
+  end
+end
