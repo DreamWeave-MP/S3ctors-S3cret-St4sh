92052801a42961bc549dc844c96e56d49ffe9748
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-03 11:23:30 -0600

    CLEANUP: Refactor silence manager not to have external dependencies and be prepped for MWSE deployment
    sqush

--- content/s3maphore/00 Core/scripts/s3/music/silenceManager.lua
+++ content/s3maphore/00 Core/scripts/s3/music/silenceManager.lua
@@ -1,10 +1,10 @@
-local async = require 'openmw.async'
-local storage = require 'openmw.storage'
-
+--- This is a little wacky as we violate normal import order rules here but it's
+--- more convenient to import things which will work across engines and then pull in other things
+--- in an API-specific manner
 local musicUtil = require 'scripts.s3.music.util'
 local INTERRUPT = require 'scripts.s3.music.enum.interruptMode'
 
-local SilenceGroup = storage.playerSection 'SettingsS3MusicSilenceConfig'
+local isOpenMW, async = pcall(require, 'openmw.async')
 
 ---@class SilenceData
 ---@field GlobalSilenceToggle boolean whether or not silence "tracks" are used
@@ -14,23 +14,29 @@ local SilenceGroup = storage.playerSection 'SettingsS3MusicSilenceConfig'
 ---@field BattleSilenceMin integer minimum duration of silence tracks for battle playlists
 ---@field BattleSilenceMax integer maximum duration of silence tracks for battle playlists
 ---@field time number current remaining duration for silence
-local silenceData = {
+local SilenceData = {
     time = 0
 }
 
-local function updateSilenceSettings(_, updated)
-    if updated then
-        silenceData[updated] = SilenceGroup:get(updated)
-    else
-        for key, value in ipairs(SilenceGroup:asTable()) do
-            silenceData[key] = value
+if isOpenMW then
+    local storage = require 'openmw.storage'
+    local SilenceGroup = storage.playerSection 'SettingsS3MusicSilenceConfig'
+
+    local function updateSilenceSettings(_, updated)
+        if updated then
+            SilenceData[updated] = SilenceGroup:get(updated)
+        else
+            for key, value in ipairs(SilenceGroup:asTable()) do
+                SilenceData[key] = value
+            end
         end
     end
+
+    updateSilenceSettings()
+
+    SilenceGroup:subscribe(async:callback(updateSilenceSettings))
+else
 end
-
-updateSilenceSettings()
-
-SilenceGroup:subscribe(async:callback(updateSilenceSettings))
 
 --- Given the currently-running playlist and settings,
 --- determine whether there should be a silence played
@@ -39,42 +45,42 @@ SilenceGroup:subscribe(async:callback(updateSilenceSettings))
 --- 2. What silence range the playlist defines
 --- 3. What silence range/chance the player's settings define
 ---@param newPlaylist S3maphorePlaylist
-function silenceData.updateSilenceParams(newPlaylist)
+function SilenceData.updateSilenceParams(newPlaylist)
     local silenceParams = newPlaylist.silenceBetweenTracks
 
-    if not silenceData.GlobalSilenceToggle then
-        silenceData.time = 0
+    if not SilenceData.GlobalSilenceToggle then
+        SilenceData.time = 0
     elseif silenceParams and math.random() <= (silenceParams.chance or 1) then
         if type(silenceParams) == 'table' then
-            silenceData.time = math.random(silenceParams.min or 0, silenceParams.max or 30)
+            SilenceData.time = math.random(silenceParams.min or 0, silenceParams.max or 30)
         else
             error(
                 ('Invalid silence parameters on playlist %s, given silence parameter %s'):format(newPlaylist.id,
                     silenceParams)
             )
         end
-    elseif math.random() <= (silenceData.GlobalSilenceChance or 1) then
+    elseif math.random() <= (SilenceData.GlobalSilenceChance or 1) then
         local playlistArchetype = newPlaylist.interruptMode
 
         if playlistArchetype == INTERRUPT.Me then
-            silenceData.time = math.random(
-                silenceData.ExploreSilenceMin,
-                silenceData.ExploreSilenceMax
+            SilenceData.time = math.random(
+                SilenceData.ExploreSilenceMin,
+                SilenceData.ExploreSilenceMax
             )
         elseif playlistArchetype == INTERRUPT.Other then
-            silenceData.time = math.random(
-                silenceData.BattleSilenceMin,
-                silenceData.BattleSilenceMax
+            SilenceData.time = math.random(
+                SilenceData.BattleSilenceMin,
+                SilenceData.BattleSilenceMax
             )
         else
             -- Special playlists must always define their own silence parameters
-            silenceData.time = 0
+            SilenceData.time = 0
         end
 
-        musicUtil.debugLog('archetypal silence time is...', silenceData.time)
+        musicUtil.debugLog('archetypal silence time is...', SilenceData.time)
     else
-        silenceData.time = 0
+        SilenceData.time = 0
     end
 end
 
-return silenceData
+return SilenceData
