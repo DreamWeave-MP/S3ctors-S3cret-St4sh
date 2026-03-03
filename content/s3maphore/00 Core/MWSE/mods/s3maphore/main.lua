2ec277993d70427e5628260c8dbcd7f82de94427
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-03 08:34:31 -0700

    Change require path for music utility module
    Updated require path for music utility module and added comments regarding its usage.

--- content/s3maphore/00 Core/MWSE/mods/s3maphore/main.lua
+++ content/s3maphore/00 Core/MWSE/mods/s3maphore/main.lua
@@ -4,4 +4,7 @@ package.path = package.path .. ";.\\Data Files\\?.lua;"
 require("s3maphore.meta")
 require("s3maphore.mcm")
 
-require("omw.music.helpers")
+--- This path was changed in current builds, but also
+--- The util module returns a local variable which should be bound to a variable in the scope in which it's require'd
+--- There is no way this works like you're expecting it to
+require("scripts.s3.music.util")
