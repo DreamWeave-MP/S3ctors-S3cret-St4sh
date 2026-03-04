4a1d9c1e2eb38b7da7cf9da4713487ee387d06ac
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-04 08:30:07 -0600

    FEAT: Add a dedicated file for the core settings, which is API agnostic and cacheable

---
+++ content/s3maphore/00 Core/scripts/s3/music/musicSettings.lua
@@ -0,0 +1,14 @@
+local musicUtil = require 'scripts.s3.music.util'
+
+---@class S3maphoreCoreSettings: UpdatingSettingTable
+---@field BannerEnabled boolean
+---@field BattleEnabled boolean
+---@field ForceFinishTrack boolean
+---@field ForcePlaylistChangeOnFriendlyExteriorTransition boolean
+---@field ForcePlaylistChangeOnHostileExteriorTransition boolean
+---@field ForcePlaylistChangeOnOverworldTransition boolean
+---@field FadeOutDuration number
+---@field MusicEnabled boolean
+local MusicSettings = musicUtil.getUpdatingSettingsTable('SettingsS3Music')
+
+return MusicSettings
