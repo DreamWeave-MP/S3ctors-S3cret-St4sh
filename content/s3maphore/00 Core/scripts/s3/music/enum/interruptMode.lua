c50f99360ba864dd5f40583a9c1b19767fbb4f75
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-04 08:42:59 -0600

    CLEANUP: Fix type annotations for interrupt modes

--- content/s3maphore/00 Core/scripts/s3/music/enum/interruptMode.lua
+++ content/s3maphore/00 Core/scripts/s3/music/enum/interruptMode.lua
@@ -7,8 +7,11 @@
 ---@field Me 0
 ---@field Other 1
 ---@field Never 2
-return require 'scripts.s3.music.util'.makeStrictReadOnly {
+local InterruptModes = require 'scripts.s3.music.util'.makeStrictReadOnly {
   Me = 0,    -- Explore
   Other = 1, -- Battle
   Never = 2, -- Special
 }
+
+---@type InterruptModes
+return InterruptModes
