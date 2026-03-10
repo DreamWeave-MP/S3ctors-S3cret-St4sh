622d946d510b1c522c75be66505d8a0a7c61c194
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-10 18:38:20 -0500

    FEAT: Add new reason states for playlist and track changes

--- content/s3maphore/00 Core/scripts/s3/music/enum/stateChangeReason.lua
+++ content/s3maphore/00 Core/scripts/s3/music/enum/stateChangeReason.lua
@@ -2,18 +2,24 @@
 ---| 'DIED'
 ---| 'DSBL'
 ---| 'NPLS'
+---| 'PLCH'
 ---| 'SPTR'
+---| 'TRCH'
 
 ---@class StateChangeReasons: StrictReadOnlyTable
 ---@field Died 'DIED'
 ---@field Disabled 'DSBL'
 ---@field NoPlaylist 'NPLS'
+---@field PlaylistChanged 'PLCH'
 ---@field SpecialTrackPlaying 'SPTR'
+---@field TrackChanged 'TRCH'
 local StateChangeReason = {
   Died = 'DIED',
   Disabled = 'DSBL',
   NoPlaylist = 'NPLS',
+  PlaylistChanged = 'PLCH',
   SpecialTrackPlaying = 'SPTR',
+  TrackChanged = 'TRCH',
 }
 
 return require 'scripts.s3.music.util'.makeReadOnly(StateChangeReason, false, true)
