5c58212cf9cb4136887980329d34e664d3568659
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-03-10 17:10:06 -0500

    FEAT: Add a main.lua for H3 to be used in MWSE

---
+++ content/h3lp_yours3lf/MWSE/mods/H3/main.lua
@@ -0,0 +1,8 @@
+-- mwse.memory.writeNoOperation{address = 0x4BB666, length = 18}
+package.path = package.path .. ";.\\Data Files\\?.lua;"
+
+math = require 'scripts.s3.math'
+NullFunction = require 'scripts.s3.nullFunction'
+table = require 'scripts.s3.table'
+
+H3Rand = require 'scripts.s3.randomGen'
