8222ef0dc6df494312fcd15a6779a154622a0c22
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-06-25 15:55:55 -0500

    FIX: Remove non-existent MWSE imports from H3

--- content/h3lp_yours3lf/MWSE/mods/H3/main.lua
+++ content/h3lp_yours3lf/MWSE/mods/H3/main.lua
@@ -1,8 +1,6 @@
 -- mwse.memory.writeNoOperation{address = 0x4BB666, length = 18}
 package.path = package.path .. ";.\\Data Files\\?.lua;"
 
-math = require 'scripts.s3.math'
 NullFunction = require 'scripts.s3.nullFunction'
-table = require 'scripts.s3.table'
 
 H3Rand = require 'scripts.s3.randomGen'
