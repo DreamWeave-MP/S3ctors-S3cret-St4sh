452dbcf55875b1894b8106a798fe94f2a96dfde5
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-06-30 18:34:38 -0500

    CLEANUP: remove unused StrFind from util.lua

--- content/static_switching_system/Scripts/staticSwitcher/util.lua
+++ content/static_switching_system/Scripts/staticSwitcher/util.lua
@@ -16,8 +16,8 @@ local LOG_PREFIX, LOG_FORMAT_STR, MISSING_MESH_ERROR, PREFIX_FRAME, TITLE_CAP_FO
 local GOBJECT_TYPE = 'MWLua::GObject'
 
 local assert, error, pairs, print, tonumber, type = assert, error, pairs, print, tonumber, type
-local StrFind, StrFormat, StrGsub, StrGmatch, StrLower, StrMatch, StrUpper, StrSub =
-		string.find, string.format, string.gsub, string.gmatch, string.lower, string.match, string.upper, string.sub
+local StrFormat, StrGsub, StrGmatch, StrLower, StrMatch, StrUpper, StrSub =
+	string.format, string.gsub, string.gmatch, string.lower, string.match, string.upper, string.sub
 
 local Insert, IsArray, Max, Floor =
 		table.insert,
