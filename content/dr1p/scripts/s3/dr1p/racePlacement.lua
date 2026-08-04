4e7cadcabbcda6d4ef2b6842319d4a911124e7a1
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-08-04 16:11:52 -0500

    FEAT: Add head and race offset placements

---
+++ content/dr1p/scripts/s3/dr1p/racePlacement.lua
@@ -0,0 +1,670 @@
+---@omw-context none
+
+local Dr1pEnum = require 'scripts.s3.dr1p.enum'
+local AuxSlot, Finger, Hand = Dr1pEnum.AuxSlot, Dr1pEnum.Finger, Dr1pEnum.Hand
+
+---@type table<string, DR1PPlacementMap>
+local RacePlacement = {
+  ['redguard'] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [AuxSlot.Belt] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+    [AuxSlot.Amulet] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+  },
+  ['dark elf'] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [AuxSlot.Belt] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+    [AuxSlot.Amulet] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+  },
+  ['imperial'] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [AuxSlot.Belt] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+    [AuxSlot.Amulet] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+  },
+  ['breton'] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [AuxSlot.Belt] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+    [AuxSlot.Amulet] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+  },
+  ['nord'] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [AuxSlot.Belt] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+    [AuxSlot.Amulet] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+  },
+  ['wood elf'] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [AuxSlot.Belt] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+    [AuxSlot.Amulet] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+  },
+  ['high elf'] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [AuxSlot.Belt] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+    [AuxSlot.Amulet] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+  },
+  ['khajiit'] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [AuxSlot.Belt] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+    [AuxSlot.Amulet] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+  },
+  ['argonian'] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [AuxSlot.Belt] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+    [AuxSlot.Amulet] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+  },
+  ['orc'] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Thumb] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Index] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Middle] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Ring] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+      [Finger.Pinky] = {
+        pos = { x = 0, y = 0, z = 0 },
+        rot = { x = 0, y = 0, z = 0 },
+        scale = { x = 1, y = 1, z = 1 },
+      },
+    },
+    [AuxSlot.Belt] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+    [AuxSlot.Amulet] = {
+      pos = { x = 0, y = 0, z = 0 },
+      rot = { x = 0, y = 0, z = 0 },
+      scale = { x = 1, y = 1, z = 1 },
+    },
+  },
+}
+
+return RacePlacement
