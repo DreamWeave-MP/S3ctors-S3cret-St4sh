a8cfaa33c60365833667c7037c1c7434a9bb967b
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-08-04 18:29:59 -0500

    FIX: Correct left index and thumb ring positions for female first person

--- content/dr1p/scripts/s3/dr1p/basePlacement.lua
+++ content/dr1p/scripts/s3/dr1p/basePlacement.lua
@@ -10,36 +10,36 @@ local Radians = math.rad
 local Placements = {
   [Skeleton.FemaleFirst] = {
     [Hand.Left] = {
-      [Finger.Index] = {
-        [BodyType.Vanilla] = {
-          pos = {
-            x = 0,
-            y = 0,
-            z = 0,
-          },
-          rot = {
-            x = Radians(0),
-            y = Radians(0),
-            z = Radians(0),
-          },
-          scale = {
-            x = 0.5,
-            y = 0.5,
-            z = 0.5,
-          },
-        },
-      },
       [Finger.Thumb] = {
         [BodyType.Vanilla] = {
           pos = {
             x = 0.6,
-            y = -0.25,
-            z = -0.20,
+            y = -0.55,
+            z = 0.10,
           },
           rot = {
-            x = Radians(195),
-            y = Radians(60),
+            x = Radians(-150),
+            y = Radians(75),
             z = Radians(-60),
+          },
+          scale = {
+            x = 0.5,
+            y = 0.55,
+            z = 0.5,
+          },
+        },
+      },
+      [Finger.Index] = {
+        [BodyType.Vanilla] = {
+          pos = {
+            x = 1,
+            y = 0.75,
+            z = -0.2,
+          },
+          rot = {
+            x = Radians(-50),
+            y = Radians(-90),
+            z = Radians(0),
           },
           scale = {
             x = 0.5,
