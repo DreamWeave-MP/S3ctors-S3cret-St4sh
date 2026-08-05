a2b13dd73d4991ec9dfa9be2ce1d096e55973a03
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-08-05 11:45:32 -0500

    FEAT: Add female third person rings

--- content/dr1p/scripts/s3/dr1p/basePlacement.lua
+++ content/dr1p/scripts/s3/dr1p/basePlacement.lua
@@ -118,6 +118,73 @@ local Placements = {
     -- },
   },
   [Skeleton.FemaleThird] = {
+    [Hand.Left] = {
+      [Finger.Thumb] = {
+        [BodyType.Vanilla] = {
+          [Transform.Position] = {
+            [Axis.X] = 0.95,
+            [Axis.Y] = -0.250,
+            [Axis.Z] = 0.05,
+          },
+
+          [Transform.Rotation] = {
+            [Axis.X] = Radians(180),
+            [Axis.Y] = Radians(90),
+            [Axis.Z] = Radians(00),
+          },
+
+          [Transform.Scale] = {
+            [Axis.X] = 0.5,
+            [Axis.Y] = 0.5,
+            [Axis.Z] = 0.5,
+          },
+        },
+      },
+      [Finger.Index] = {
+        [BodyType.Vanilla] = {
+          [Transform.Position] = {
+            [Axis.X] = 1.25,
+            [Axis.Y] = -0.00,
+            [Axis.Z] = 1.00,
+          },
+
+          [Transform.Rotation] = {
+            [Axis.X] = Radians(00),
+            [Axis.Y] = Radians(-90),
+            [Axis.Z] = Radians(0),
+          },
+
+          [Transform.Scale] = {
+            [Axis.X] = 0.40,
+            [Axis.Y] = 0.40,
+            [Axis.Z] = 0.40,
+          },
+        },
+      },
+    },
+    [Hand.Right] = {
+      [Finger.Index] = {
+        [BodyType.Vanilla] = {
+          [Transform.Position] = {
+            [Axis.X] = 1.25,
+            [Axis.Y] = -0.325,
+            [Axis.Z] = -1.00,
+          },
+
+          [Transform.Rotation] = {
+            [Axis.X] = Radians(00),
+            [Axis.Y] = Radians(-90),
+            [Axis.Z] = Radians(0),
+          },
+
+          [Transform.Scale] = {
+            [Axis.X] = 0.40,
+            [Axis.Y] = 0.40,
+            [Axis.Z] = 0.40,
+          },
+        },
+      },
+    },
     [AuxSlot.Amulet] = {
       [BodyType.Vanilla] = {
         [Transform.Position] = {
