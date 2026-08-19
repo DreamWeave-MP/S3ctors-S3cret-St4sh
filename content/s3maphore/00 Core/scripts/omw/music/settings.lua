d8a67d3e175e6aa5adf771af7de19d5331863c36
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-08-19 17:23:09 -0500

    CLEANUP: Remove playlist activity sections from the MCM because they suck and are confusing

--- content/s3maphore/00 Core/scripts/omw/music/settings.lua
+++ content/s3maphore/00 Core/scripts/omw/music/settings.lua
@@ -2,48 +2,8 @@
 
 local async = require 'openmw.async'
 local storage = require 'openmw.storage'
-local vfs = require 'openmw.vfs'
 
 local I = require 'openmw.interfaces'
-
-local PlaylistFileNames = {}
-for fileName in vfs.pathsWithPrefix 'playlists/' do
-  if fileName:find '%.lua$' then table.insert(PlaylistFileNames, fileName) end
-end
-
-local playlistIds = {}
-for _, file in ipairs(PlaylistFileNames) do
-  local ok, playlists = pcall(require, file:gsub('%.lua$', ''))
-  if ok and type(playlists) == 'table' then
-    for _, playlist in ipairs(playlists) do
-      playlistIds[#playlistIds + 1] = playlist.id
-    end
-  end
-end
-
-local function getMaxLength(arr)
-  local max = 0
-  for _, str in ipairs(arr) do
-    if #str > max then max = #str end
-  end
-  return max + 8
-end
-
-local function padStrings(arr)
-  local maxLen = getMaxLength(arr)
-  local padded = {}
-  for i, str in ipairs(arr) do
-    local totalPad = maxLen - #str
-    local leftPad = math.floor(totalPad / 2)
-    local rightPad = totalPad - leftPad
-    padded[i] = string.rep(' ', leftPad) .. str .. string.rep(' ', rightPad)
-  end
-  return padded
-end
-
-local function stripWhitespace(str) return str:match '^%s*(.-)%s*$' end
-
-playlistIds = padStrings(playlistIds)
 
 I.Settings.registerPage {
   key = 'S3Music',
@@ -157,98 +117,6 @@ I.Settings.registerGroup {
   },
 }
 
-I.Settings.registerGroup {
-  key = 'SettingsS3MusicPlaylistSelection',
-  page = 'S3Music',
-  l10n = 'S3Music',
-  name = 'PlaylistSelection',
-  permanentStorage = true,
-  order = 1,
-  settings = {
-    {
-      key = 'PlaylistActiveCurrentSelection',
-      renderer = 'select',
-      argument = { items = playlistIds, l10n = 'S3Music' },
-      name = 'CurrentPlaylistSelection',
-      description = 'CurrentPlaylistSelectionDescription',
-      default = playlistIds[1],
-    },
-    {
-      key = 'ResetAllPlaylists',
-      renderer = 'checkbox',
-      name = 'ResetAllPlaylists',
-      argument = {
-        trueLabel = 'ResetButtonLabel',
-        falseLabel = 'ResetButtonLabel',
-        l10n = 'S3Music',
-      },
-      description = 'ResetAllPlaylistsDescription',
-      default = false,
-    },
-  },
-}
-
-local musicSettings = storage.playerSection 'SettingsS3MusicPlaylistSelection'
-local activePlaylistSettings = storage.playerSection 'SettingsS3MusicPlaylistActivity'
-local activePlaylistState = storage.playerSection 'S3maphoreActivePlaylistSettings'
-
-musicSettings:subscribe(async:callback(function(_, key)
-  if not key or key == 'ResetAllPlaylists' then
-    for _, file in ipairs(PlaylistFileNames) do
-      local ok, playlists = pcall(require, file:gsub('%.lua$', ''))
-
-      if ok and type(playlists) == 'table' then
-        for _, playlist in ipairs(playlists) do
-          activePlaylistState:set(playlist.id .. 'Active', playlist.active or true)
-          activePlaylistSettings:set('PlaylistActiveState', playlist.active or true)
-        end
-      end
-    end
-    return
-  end
-
-  local targetPlaylist = stripWhitespace(musicSettings:get 'PlaylistActiveCurrentSelection')
-
-  if not targetPlaylist then return end
-
-  local currentState = activePlaylistState:get(targetPlaylist .. 'Active')
-  local noTracks = not currentState
-
-  activePlaylistSettings:set('PlaylistActiveState', noTracks and false or currentState)
-
-  I.Settings.updateRendererArgument(
-    'SettingsS3MusicPlaylistActivity',
-    'PlaylistActiveState',
-    { disabled = noTracks }
-  )
-end))
-
-I.Settings.registerGroup {
-  key = 'SettingsS3MusicPlaylistActivity',
-  page = 'S3Music',
-  l10n = 'S3Music',
-  name = 'PlaylistActivity',
-  permanentStorage = true,
-  order = 2,
-  settings = {
-    {
-      key = 'PlaylistActiveState',
-      renderer = 'checkbox',
-      argument = {},
-      name = 'PlaylistActiveState',
-      description = 'PlaylistActiveStateDescription',
-      default = true,
-    },
-  },
-}
-
-activePlaylistSettings:subscribe(async:callback(function(_, key)
-  if not key then return end
-  local targetPlaylist = stripWhitespace(musicSettings:get 'PlaylistActiveCurrentSelection')
-  local state = activePlaylistSettings:get 'PlaylistActiveState'
-  activePlaylistState:set(targetPlaylist .. 'Active', state)
-end))
-
 local HUGE = math.huge
 
 I.Settings.registerGroup {
@@ -257,7 +125,7 @@ I.Settings.registerGroup {
   l10n = 'S3Music',
   name = 'SilenceConfiguration',
   permanentStorage = true,
-  order = 3,
+  order = 1,
   settings = {
     {
       key = 'GlobalSilenceToggle',
