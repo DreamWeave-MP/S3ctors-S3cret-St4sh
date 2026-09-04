73297a8b61d52c2996b17ed624c05c4650cdb50d
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-09-04 10:36:23 -0500

    CLEANUP: These are actually on the base object

--- content/cod3x/openmw_object_types.lua
+++ content/cod3x/openmw_object_types.lua
@@ -28,6 +28,7 @@ local ObjectList = {}
 ---@field position openmw.util.Vector3 Object position.
 ---@field scale number Object scale.
 ---@field rotation openmw.util.Transform Object rotation.
+---@field saveState boolean Whether this object has been modified and will be written to the save game. Global scripts can set the value; local scripts are read-only.
 ---@field startingCell openmw.core.Cell? The object's original cell. Returns nil if `cell` of the object is nil.
 ---@field startingPosition openmw.util.Vector3 The object original position
 ---@field startingRotation openmw.util.Transform The object original rotation
@@ -51,7 +52,6 @@ local LObject = {}
 ---@field startingCell openmw.core.GCell? The object's original cell. Returns nil if `cell` of the object is nil.
 ---@field cell openmw.core.GCell? The cell where the object currently is. During loading a game and for objects in an inventory or a container `cell` is nil.
 ---@field enabled boolean Whether the object is enabled or disabled. Global scripts can set the value. Items in containers or inventories can't be disabled.
----@field saveState boolean Whether this object has been modified and will be written to the save game. Global scripts can set the value; local scripts are read-only.
 local GObject = {}
 
 ---The object handle for the object a local script is attached to.
