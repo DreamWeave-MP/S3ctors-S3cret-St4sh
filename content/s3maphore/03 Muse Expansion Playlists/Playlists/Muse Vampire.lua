c1767590d1a209f9f6f67da4d050f0a60aceb91f
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-09-04 22:59:46 -0500

    FEAT: Add support for CaptainCreepy's vampire module

---
+++ content/s3maphore/03 Muse Expansion Playlists/Playlists/Muse Vampire.lua
@@ -0,0 +1,31 @@
+---@module 'doc.playlistEnv'
+
+---@type IDPresenceMap
+local VampireBossNames = {
+  ['raxle berne'] = true,
+  ['volrina quarra'] = true,
+  ['dhaunayne aundae'] = true,
+  ['akavorioc'] = true,
+  ['zargoryn thalen'] = true,
+  ['ancasarion'] = true,
+  ['alnemthirh'] = true,
+  ['njelfa'] = true,
+  ['armennu'] = true,
+}
+
+---@type ValidPlaylistCallback
+local function vampireBossRule(playback)
+  return playback.state.isInCombat and playback.rules.combatTargetExact(VampireBossNames)
+end
+
+---@type S3maphorePlaylist[]
+return {
+  {
+    id = 'ms/combat/vampire',
+    priority = PlaylistPriority.BattleMod - 1,
+    randomize = true,
+    fadeOut = 0,
+
+    isValidCallback = vampireBossRule,
+  },
+}
