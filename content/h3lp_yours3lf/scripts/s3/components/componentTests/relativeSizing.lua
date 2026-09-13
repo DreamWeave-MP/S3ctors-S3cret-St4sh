e3c0092d4c7b4dc85e7df22092917b3cd9746d28
Dave Corley <corleycomputerrepair@protonmail.ch>
2026-09-13 02:26:46 -0500

    FIX: RelativeSize test

--- content/h3lp_yours3lf/scripts/s3/components/componentTests/relativeSizing.lua
+++ content/h3lp_yours3lf/scripts/s3/components/componentTests/relativeSizing.lua
@@ -3,7 +3,6 @@
 local I = require 'openmw.interfaces'
 local util = require 'openmw.util'
 
-local box = require 'scripts.s3.components.box'
 local column = require 'scripts.s3.components.column'
 local constants = require 'scripts.omw.mwui.constants'
 local meter = require 'scripts.s3.components.meter'
@@ -11,15 +10,19 @@ local row = require 'scripts.s3.components.row'
 local slider = require 'scripts.s3.components.slider'
 local spacer = require 'scripts.s3.components.spacer'
 local text = require 'scripts.s3.components.text'
+local widget = require 'scripts.s3.components.widget'
 
 local UtilVector2 = util.vector2
+local fullSize = UtilVector2(1, 1)
 local fullWidth = UtilVector2(1, 0)
 local halfWidth = UtilVector2(0.5, 0)
 local fixedPanelSize = UtilVector2(520, 150)
 local relativeSliderSize = UtilVector2(-40, 18)
 local relativeMeterSize = UtilVector2(-40, 16)
 local halfMeterSize = UtilVector2(-12, 16)
+local rowSize = UtilVector2(0, 16)
 local gap = UtilVector2(12, 0)
+
 local headerTextProps = {
   textColor = constants.headerColor,
   textSize = constants.textHeaderSize,
@@ -28,19 +31,28 @@ local function refresh() I.H3ComponentTest.refresh() end
 
 ---@return openmw.ui.Layout
 local function relativeSizing()
-  return box {
+  return widget {
     name = 'ct_demo_relative_sizing',
+
     props = { size = fixedPanelSize },
+
     children = {
       column {
+        props = {
+          autoSize = false,
+          relativeSize = fullSize,
+        },
+
         children = {
           text {
-            text = 'Relative sizing + explicit slider trackWidth',
+            text = 'Relative sizing in a fixed Widget coordinate space',
             props = headerTextProps,
           },
+
           text {
             text = 'Drag the slider to verify redraw with a relative-width track.',
           },
+
           slider {
             value = 25,
             min = 0,
@@ -63,6 +75,12 @@ local function relativeSizing()
           },
 
           row {
+            props = {
+              autoSize = false,
+              relativeSize = fullWidth,
+              size = rowSize,
+            },
+
             children = {
               meter {
                 value = 25,
